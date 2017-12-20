773f11e147d347ef8942cb2d8b5387bf1a1828a7
Kay-Michael Würzner <wuerzner@gmail.com>
2017-12-20 14:24:37 +0100

    * load images

--- ocrd/init/initializing.py
+++ ocrd/init/initializing.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
 import xml.etree.ElementTree as ET
-import urllib.request
+import requests
 
 ns = { 'mets'  : "http://www.loc.gov/METS/",
        'mods'  : "http://www.loc.gov/mods/v3",
@@ -24,6 +25,9 @@ class Initializer:
 
         self.tree = ET.ElementTree()
         self.set_working_dir("./")
+        self.img_src = {}
+        self.img_files = {}
+        self.fulltext = {}
 
     def set_working_dir(self,path):
         """
@@ -51,12 +55,13 @@ class Initializer:
         PAGE XML files are either crawled or created and copied to the WD.
         """
 
-        self._load_and_create_page()
+        self._load_images()
+        self._load_or_create_page()
 
 
-    def _load_and_create_page(self):
+    def _load_or_create_page(self):
         """
-        Loads and creates missing PAGE XML from internal METS tree.
+        Loads or creates missing PAGE XML from internal METS tree.
         """
         page_fileGrps = self.tree.getroot().findall(".//mets:fileGrp[@USE='FULLTEXT']", ns)
         # case load page
@@ -66,3 +71,25 @@ class Initializer:
         # case create page TODO
         else:
             pass
+
+    def _load_images(self):
+        """
+        Retrieves images referenced in the METS and copies them to the WD.
+        """
+        img_fileGrps = self.tree.getroot().findall(".//mets:fileGrp[@USE='IMAGE']", ns)
+        for img_fileGrp in img_fileGrps:
+            img_files = img_fileGrp.findall("./mets:file", ns)
+            for img_file in img_files:
+                # extract information from elem
+                img_ID = img_file.get("ID")
+                ID = img_ID.rstrip("_IMAGE")
+                img_url = img_file.find("mets:FLocat", ns).get("{%s}href" % ns["xlink"])
+
+                # make a local copy
+                img_data = requests.get(img_url)
+                if img_data.status_code == 200:
+                    self.img_src[ID] = img_url
+                    self.img_files[ID] = "%s/%s" % (self.working_dir, os.path.basename(img_url))
+                    with open(self.img_files[ID], 'wb') as f:  
+                        f.write(img_data.content)
+                
