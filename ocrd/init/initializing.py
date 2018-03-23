48c9bb1aa97a634e3461ede28eb6c72f552a0004
Konstantin Baierer <unixprog@gmail.com>
2018-03-23 12:39:32 +0100

    :art: autopep8 and pylint init

--- ocrd/init/initializing.py
+++ ocrd/init/initializing.py
@@ -5,13 +5,14 @@ import requests
 
 from lxml import etree as ET
 
-ns = { 'mets'  : "http://www.loc.gov/METS/",
-       'mods'  : "http://www.loc.gov/mods/v3",
-       'xlink' : "http://www.w3.org/1999/xlink",
-       'page'  : "http://schema.primaresearch.org/PAGE/gts/pagecontent/2017-07-15",
+ns = {'mets': "http://www.loc.gov/METS/",
+      'mods': "http://www.loc.gov/mods/v3",
+      'xlink': "http://www.w3.org/1999/xlink",
+      'page': "http://schema.primaresearch.org/PAGE/gts/pagecontent/2017-07-15",
      }
 
-class Handle:
+
+class Handle(object):
     """
     Internal data structure.
     """
@@ -28,14 +29,15 @@ class Handle:
         Resets the Initializer.
         """
 
-        self.tree = ET.ElementTree()
+        self.tree = ET.ElementTree() # pylint: disable=no-member
         self.img_src = {}
         self.img_files = {}
         self.page_src = {}
         self.page_files = {}
         self.page_trees = {}
 
-class Initializer:
+
+class Initializer(object):
     """
     Initializes an OCR process given a METS XML file.
     """
@@ -45,6 +47,7 @@ class Initializer:
         The constructor.
         """
 
+        self.working_dir = None
         self.clear()
 
     def clear(self):
@@ -55,19 +58,19 @@ class Initializer:
         self.set_working_dir("./")
         self.handle = Handle()
 
-    def set_working_dir(self,path):
+    def set_working_dir(self, path):
         """
         (Re)sets the working directory.
         """
         self.working_dir = path
 
-    def load_string(self,mets_xml):
+    def load_string(self, mets_xml):
         """
         Loads METS XML from a string.
         """
         pass
 
-    def load(self,mets_xml_file):
+    def load(self, mets_xml_file):
         """
         Loads METS XML from a file (i.e. file name).
         """
@@ -84,12 +87,12 @@ class Initializer:
         self._load_images()
         self._load_or_create_page()
 
-
     def _load_or_create_page(self):
         """
         Loads or creates missing PAGE XML from internal METS tree.
         """
-        page_fileGrps = self.handle.tree.getroot().findall(".//mets:fileGrp[@USE='FULLTEXT']", ns)
+        page_fileGrps = self.handle.tree.getroot().findall(
+            ".//mets:fileGrp[@USE='FULLTEXT']", ns)
         # case load page
         if page_fileGrps:
             for page_fileGrp in page_fileGrps:
@@ -98,18 +101,21 @@ class Initializer:
                     # extract information from elem
                     page_ID = page_file.get("ID")
                     ID = page_ID.rstrip("_FULLTEXT")
-                    page_url = page_file.find("mets:FLocat", ns).get("{%s}href" % ns["xlink"])
+                    page_url = page_file.find("mets:FLocat", ns).get(
+                        "{%s}href" % ns["xlink"])
 
                     # make a local copy
                     page_data = requests.get(page_url)
                     if page_data.status_code == 200:
                         self.handle.page_src[ID] = page_url
-                        self.handle.page_files[ID] = "%s/%s" % (self.working_dir, os.path.basename(page_url))
-                        with open(self.handle.page_files[ID], 'wb') as f:  
+                        self.handle.page_files[ID] = "%s/%s" % (
+                            self.working_dir, os.path.basename(page_url))
+                        with open(self.handle.page_files[ID], 'wb') as f:
                             f.write(page_data.content)
                         # parse the page xml (and store the tree to avoid repeated parsing)
-                        self.handle.page_trees[ID] = ET.ElementTree()
-                        self.handle.page_trees[ID].parse(self.handle.page_files[ID])
+                        self.handle.page_trees[ID] = ET.ElementTree()  # pylint: disable=no-member
+                        self.handle.page_trees[ID].parse(
+                            self.handle.page_files[ID])
         # case create page TODO
         else:
             pass
@@ -118,21 +124,24 @@ class Initializer:
         """
         Retrieves images referenced in the METS and copies them to the WD.
         """
-        img_fileGrps = self.handle.tree.getroot().findall(".//mets:fileGrp[@USE='IMAGE']", ns)
+        img_fileGrps = self.handle.tree.getroot().findall(
+            ".//mets:fileGrp[@USE='IMAGE']", ns)
         for img_fileGrp in img_fileGrps:
             img_files = img_fileGrp.findall("./mets:file", ns)
             for img_file in img_files:
                 # extract information from elem
                 img_ID = img_file.get("ID")
                 ID = img_ID.rstrip("_IMAGE")
-                img_url = img_file.find("mets:FLocat", ns).get("{%s}href" % ns["xlink"])
+                img_url = img_file.find("mets:FLocat", ns).get(
+                    "{%s}href" % ns["xlink"])
 
                 # make a local copy
                 img_data = requests.get(img_url)
                 if img_data.status_code == 200:
                     self.handle.img_src[ID] = img_url
-                    self.handle.img_files[ID] = "%s/%s" % (self.working_dir, os.path.basename(img_url))
-                    with open(self.handle.img_files[ID], 'wb') as f:  
+                    self.handle.img_files[ID] = "%s/%s" % (
+                        self.working_dir, os.path.basename(img_url))
+                    with open(self.handle.img_files[ID], 'wb') as f:
                         f.write(img_data.content)
 
     def get_handle(self):
