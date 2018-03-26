80d33da9deccec9547453febdb73ff5b42217f22
Konstantin Baierer <unixprog@gmail.com>
2018-03-26 11:47:51 +0200

    test assets

---
+++ test/assets/__init__.py
@@ -0,0 +1,7 @@
+from test.base import PWD
+
+_PREFIX = 'file://' + PWD + '/'
+
+METS_HEROLD = _PREFIX + 'assets/herold/mets.xml'
+METS_HEROLD_SMALL = _PREFIX + 'assets/herold/mets_one_file.xml'
+METS_HEROLD_PAGE_5 = _PREFIX + 'assets/herold/00000005.xml'
