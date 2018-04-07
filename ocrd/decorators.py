8142bb13500281944f7b5e074a73259137404e8b
Konstantin Baierer <unixprog@gmail.com>
2018-04-08 00:27:12 +0200

    decorators: ocrd_cli_wrap_processor and ocrd_cli_options

---
+++ ocrd/decorators.py
@@ -0,0 +1,40 @@
+import os
+
+import click
+
+from ocrd.resolver import Resolver
+from ocrd.processor.base import run_processor
+
+def ocrd_cli_wrap_processor(processor, mets_url=None, cache_enabled=True, *args, **kwargs):
+    if mets_url.find('://') == -1:
+        mets_url = 'file://' + mets_url
+    if mets_url.startswith('file://') and not os.path.exists(mets_url[len('file://'):]):
+        raise Exception("File does not exist: %s" % mets_url)
+    resolver = Resolver(cache_enabled=cache_enabled)
+    workspace = resolver.workspace_from_url(mets_url)
+    run_processor(processor, mets_url, workspace=workspace, *args, **kwargs)
+
+def ocrd_cli_options(f):
+    """
+    Implement MP CLI.
+
+    Usage::
+
+        import ocrd_click_cli from ocrd.utils
+
+        @click.command()
+        @ocrd_click_cli
+        def cli(mets_url):
+            print(mets_url)
+    """
+    params = [
+        click.option('-m', '--mets-url', help="METS URL to validate", required=True),
+        click.option('-o', '--output-mets', help="METS URL to write resulting METS to"),
+        click.option('-p', '--parameter', type=click.Path()),
+        click.option('-w', '--workingDir', help="Working Directory"),
+        click.option('-I', '--inputFileGrp', help='File group(s) used as input.'),
+        click.option('-O', '--outputFileGrp', help='File group(s) used as output.'),
+    ]
+    for param in params:
+        param(f)
+    return f
