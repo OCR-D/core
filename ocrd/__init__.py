8be7cc66019dc363f7f7ffb693126e2d4df7d604
Konstantin Baierer <unixprog@gmail.com>
2018-04-08 05:05:45 +0200

    export constants, processor and model members from 'ocrd'

---
+++ ocrd/__init__.py
@@ -0,0 +1,3 @@
+from ocrd.processor.base import run_processor, run_cli, Processor
+from ocrd.model import OcrdPage, OcrdMets, OcrdExif, OcrdFile
+from ocrd.constants import * # pylint: disable=wildcard-import
--- ocrd/model/__init__.py
+++ ocrd/model/__init__.py
@@ -2,3 +2,4 @@ from .ocrd_xml_base import OcrdXmlDocument
 from .ocrd_mets import OcrdMets
 from .ocrd_page import OcrdPage
 from .ocrd_exif import OcrdExif
+from .ocrd_file import OcrdFile
