64d20aff865480c098983cddcdd074b7817871e8
Konstantin Baierer <unixprog@gmail.com>
2018-04-19 16:57:45 +0200

    0.0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='ocrd',
-    version='0.0.5',
+    version='0.0.6',
     description='OCR-D framework',
     long_description=README,
     author='Kay-Michael Würzner, Konstantin Baierer',
