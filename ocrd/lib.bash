2a9f954da0f108127bcf5ec93c7f86f67cad8203
Konstantin Baierer <unixprog@gmail.com>
2018-06-22 04:01:03 +0200

    bashlib: declare OCRD_TOOL_JSON OCRD_TOOL_NAME argv params in bashlib, add ocrd__wrap

--- ocrd/lib.bash
+++ ocrd/lib.bash
@@ -108,20 +108,33 @@ ocrd__parse_argv () {
 }
 
 # END-INCLUDE 
+# BEGIN-INCLUDE ./src/wrap.bash 
+ocrd__wrap () {
 
-if ! which "ocrd" >/dev/null 2>/dev/null;then
-    ocrd__raise "ocrd not in \$PATH"
-fi
+    declare -gx OCRD_TOOL_JSON="$1"
+    declare -gx OCRD_TOOL_NAME="$2"
+    shift
+    shift
+    declare -Agx params argv
 
-if ! declare -p "OCRD_TOOL_JSON" >/dev/null 2>/dev/null;then
-    ocrd__raise "Must set \$OCRD_TOOL_JSON"
-elif [[ ! -r "$OCRD_TOOL_JSON" ]];then
-    ocrd__raise "Cannot read \$OCRD_TOOL_JSON: '$OCRD_TOOL_JSON'"
-fi
+    if ! which "ocrd" >/dev/null 2>/dev/null;then
+        ocrd__raise "ocrd not in \$PATH"
+    fi
 
-if [[ -z "$OCRD_TOOL_NAME" ]];then
-    ocrd__raise "Must set \$OCRD_TOOL_NAME"
-elif ! ocrd ocrd-tool "$OCRD_TOOL_JSON" list-tools|grep -q "$OCRD_TOOL_NAME";then
-    ocrd__raise "No such command \$OCRD_TOOL_NAME: $OCRD_TOOL_NAME"
-fi
+    if ! declare -p "OCRD_TOOL_JSON" >/dev/null 2>/dev/null;then
+        ocrd__raise "Must set \$OCRD_TOOL_JSON"
+    elif [[ ! -r "$OCRD_TOOL_JSON" ]];then
+        ocrd__raise "Cannot read \$OCRD_TOOL_JSON: '$OCRD_TOOL_JSON'"
+    fi
 
+    if [[ -z "$OCRD_TOOL_NAME" ]];then
+        ocrd__raise "Must set \$OCRD_TOOL_NAME"
+    elif ! ocrd ocrd-tool "$OCRD_TOOL_JSON" list-tools|grep -q "$OCRD_TOOL_NAME";then
+        ocrd__raise "No such command \$OCRD_TOOL_NAME: $OCRD_TOOL_NAME"
+    fi
+
+    ocrd__parse_argv "$@"
+
+}
+
+# END-INCLUDE 
