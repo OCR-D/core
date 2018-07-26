015832233000e89619bc3f0edc9dd9e2196aba3f
Konstantin Baierer <unixprog@gmail.com>
2018-07-26 16:07:44 +0200

    replace exiftool with native python, fix #71

--- ocrd/utils.py
+++ ocrd/utils.py
@@ -21,6 +21,7 @@ logging.basicConfig(level=logging.DEBUG)
 #  logging.getLogger('ocrd.resolver').setLevel(logging.INFO)
 logging.getLogger('ocrd.resolver.download_to_directory').setLevel(logging.INFO)
 logging.getLogger('ocrd.resolver.add_files_to_mets').setLevel(logging.INFO)
+logging.getLogger('PIL').setLevel(logging.INFO)
 
 def getLogger(*args, **kwargs):
     return logging.getLogger(*args, **kwargs)
--- test/model/test_exif.py
+++ test/model/test_exif.py
@@ -1,4 +1,6 @@
 from test.base import TestCase, main, assets
+from PIL import Image
+
 from ocrd.model import OcrdExif
 
 # pylint: disable=no-member
@@ -10,13 +12,25 @@ class TestOcrdExif(TestCase):
         self.assertEqual(exif.height, 3749)
         self.assertEqual(exif.xResolution, 300)
         self.assertEqual(exif.yResolution, 300)
+        self.assertEqual(exif.compression, 'jpeg')
+        self.assertEqual(exif.photometricInterpretation, 'RGB')
 
-    def test_png(self):
+    def test_png1(self):
         exif = OcrdExif.from_filename(assets.path_to('kant_aufklaerung_1784-binarized/kant_aufklaerung_1784_0020.bin.png'))
         self.assertEqual(exif.width, 1457)
         self.assertEqual(exif.height, 2084)
-        self.assertEqual(exif.xResolution, 0)
-        self.assertEqual(exif.yResolution, 0)
+        self.assertEqual(exif.xResolution, -1)
+        self.assertEqual(exif.yResolution, -1)
+        self.assertEqual(exif.compression, None)
+        self.assertEqual(exif.photometricInterpretation, 'L')
+
+    def test_png2(self):
+        exif = OcrdExif.from_filename(assets.path_to('scribo-test/orig.sauvola.png'))
+        self.assertEqual(exif.width, 2097)
+        self.assertEqual(exif.height, 3062)
+        self.assertEqual(exif.xResolution, -1)
+        self.assertEqual(exif.yResolution, -1)
+        self.assertEqual(exif.photometricInterpretation, '1')
 
     def test_jpg(self):
         exif = OcrdExif.from_filename(assets.path_to('leptonica_samples/1555.007.jpg'))
@@ -24,13 +38,15 @@ class TestOcrdExif(TestCase):
         self.assertEqual(exif.height, 1472)
         self.assertEqual(exif.xResolution, 1)
         self.assertEqual(exif.yResolution, 1)
+        self.assertEqual(exif.photometricInterpretation, 'RGB')
 
     def test_jp2(self):
         exif = OcrdExif.from_filename(assets.path_to('kant_aufklaerung_1784-jp2/kant_aufklaerung_1784_0020.jp2'))
         self.assertEqual(exif.width, 1457)
         self.assertEqual(exif.height, 2084)
-        #  self.assertEqual(exif.xResolution, 1)
-        #  self.assertEqual(exif.yResolution, 1)
+        self.assertEqual(exif.xResolution, -1)
+        self.assertEqual(exif.yResolution, -1)
+        self.assertEqual(exif.photometricInterpretation, 'RGB')
 
 if __name__ == '__main__':
     main()
