fce5a4b996d851d82eba74aa32c659ba4d3ef66c
Konstantin Baierer <unixprog@gmail.com>
2018-08-16 17:24:14 +0200

    bashlib: zsh compat changes

--- ocrd/lib.bash
+++ ocrd/lib.bash
@@ -54,15 +54,15 @@ Options:
 # BEGIN-INCLUDE ./src/parse_argv.bash 
 ## ### `ocrd__parse_argv`
 ## 
-## Expects an associative array ("hash"/"dict") `argv` to be defined:
+## Expects an associative array ("hash"/"dict") `ocrd__argv` to be defined:
 ## 
 ## ```sh
-## declare -A argv=()
+## declare -A ocrd__argv=()
 ## ```
 ocrd__parse_argv () {
 
-    if ! declare -p "argv" >/dev/null 2>/dev/null ;then
-        ocrd__raise "Must set \$argv (declare -A argv)"
+    if ! declare -p "ocrd__argv" >/dev/null 2>/dev/null ;then
+        ocrd__raise "Must set \$ocrd__argv (declare -A ocrd__argv)"
     fi
 
     if ! declare -p "params" >/dev/null 2>/dev/null ;then
@@ -71,42 +71,42 @@ ocrd__parse_argv () {
 
     while [[ "${1:-}" = -* ]];do
         case "$1" in
-            -l|--log-level) argv['log_level']=$2 ; shift ;;
+            -l|--log-level) ocrd__argv['log_level']=$2 ; shift ;;
             -h|--help|--usage) ocrd__usage; exit ;;
             -J|--dump-json) ocrd__dumpjson; exit ;;
-            -p|--parameter) argv['parameter']=$2 ; shift ;;
-            -g|--group-id) argv['group_id']=$2 ; shift ;;
-            -O|--output-file-grp) argv['output_file_grp']=$2 ; shift ;;
-            -I|--input-file-grp) argv['input_file_grp']=$2 ; shift ;;
-            -w|--working-dir) argv['working_dir']=$2 ; shift ;;
-            -m|--mets-file) argv['mets_file']=$2 ; shift ;;
+            -p|--parameter) ocrd__argv['parameter']=$2 ; shift ;;
+            -g|--group-id) ocrd__argv['group_id']=$2 ; shift ;;
+            -O|--output-file-grp) ocrd__argv['output_file_grp']=$2 ; shift ;;
+            -I|--input-file-grp) ocrd__argv['input_file_grp']=$2 ; shift ;;
+            -w|--working-dir) ocrd__argv['working_dir']=$2 ; shift ;;
+            -m|--mets-file) ocrd__argv['mets_file']=$2 ; shift ;;
             -V|--version) ocrd ocrd-tool "$OCRD_TOOL_JSON" version; exit ;;
             *) ocrd__raise "Unknown option '$1'" ;;
         esac
         shift
     done
 
-    if [[ "${argv[mets_file]:-}" = "" ]];then
+    if [[ "${ocrd__argv[mets_file]:-}" = "" ]];then
         ocrd__raise "Option -m/--mets-file required"
     fi
 
-    if [[ "${argv[working_dir]:-}" = "" ]];then
-        argv[working_dir]=$(dirname "${argv[mets_file]}")
+    if [[ "${ocrd__argv[working_dir]:-}" = "" ]];then
+        ocrd__argv[working_dir]=$(dirname "${ocrd__argv[mets_file]}")
     fi
 
-    if [[ "${argv[log_level]:-}" = "" ]];then
-        argv[log_level]="INFO"
+    if [[ "${ocrd__argv[log_level]:-}" = "" ]];then
+        ocrd__argv[log_level]="INFO"
     fi
 
-    if [[ "${argv[input_file_grp]:-}" = "" ]];then
-        argv[input_file_grp]="OCR-D-IMG"
+    if [[ "${ocrd__argv[input_file_grp]:-}" = "" ]];then
+        ocrd__argv[input_file_grp]="OCR-D-IMG"
     fi
 
-    if [[ "${argv[output_file_grp]:-}" = "" ]];then
-        argv[output_file_grp]="OCR-D-TEXT"
+    if [[ "${ocrd__argv[output_file_grp]:-}" = "" ]];then
+        ocrd__argv[output_file_grp]="OCR-D-TEXT"
     fi
 
-    local params_parsed=$(ocrd ocrd-tool "$OCRD_TOOL_JSON" tool $OCRD_TOOL_NAME parse-params -p "${argv[parameter]}")
+    local params_parsed=$(ocrd ocrd-tool "$OCRD_TOOL_JSON" tool $OCRD_TOOL_NAME parse-params -p "${ocrd__argv[parameter]}")
     if [[ $? != 0 ]];then
         echo "Error: Failed to parse parameters:"
         echo "$params_parsed"
@@ -124,7 +124,8 @@ ocrd__wrap () {
     declare -gx OCRD_TOOL_NAME="$2"
     shift
     shift
-    declare -Agx params=() argv=()
+    declare -Agx params
+    declare -Agx ocrd__argv
 
     if ! which "ocrd" >/dev/null 2>/dev/null;then
         ocrd__raise "ocrd not in \$PATH"
