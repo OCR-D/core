7a9850e6138dfbb88577e2be301c702fe4924f79
Konstantin Baierer <unixprog@gmail.com>
2018-09-19 15:39:57 +0200

    test: test logging override

---
+++ test/test_logging.py
@@ -0,0 +1,30 @@
+import logging
+from test.base import TestCase, main
+
+from ocrd.utils import (
+    getLevelName,
+    setOverrideLogLevel,
+)
+
+class TestLogging(TestCase):
+
+    #  def runTest(self):
+
+    def test_getLevelName(self):
+        self.assertEqual(getLevelName('ERROR'), logging.ERROR)
+        self.assertEqual(getLevelName('FATAL'), logging.ERROR)
+        self.assertEqual(getLevelName('OFF'), logging.CRITICAL)
+
+    def test_setOverrideLogLevel(self):
+        rootLogger = logging.getLogger('')
+        somelogger = logging.getLogger('foo.bar')
+        somelogger.setLevel(getLevelName('INFO'))
+        self.assertEqual(somelogger.getEffectiveLevel(), logging.INFO)
+        setOverrideLogLevel('ERROR')
+        self.assertEqual(rootLogger.getEffectiveLevel(), logging.ERROR)
+        self.assertEqual(somelogger.getEffectiveLevel(), logging.ERROR)
+        notherlogger = logging.getLogger('bar.foo')
+        self.assertEqual(notherlogger.getEffectiveLevel(), logging.ERROR)
+
+if __name__ == '__main__':
+    main()
