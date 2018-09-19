8b99b51214e9b4b692a773ff07e2b8a39c179d6c
Konstantin Baierer <unixprog@gmail.com>
2018-09-19 18:29:11 +0200

    logging/cli: test click decorators

---
+++ test/test_cli.py
@@ -0,0 +1,35 @@
+from test.base import TestCase, main
+import click
+from click.testing import CliRunner
+from ocrd.decorators import ocrd_cli_options
+
+@click.command()
+@ocrd_cli_options
+def cli(*args, **kwargs): # pylint: disable=unused-argument
+    pass
+
+class TestCli(TestCase):
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+    def test_minimal(self):
+        result = self.runner.invoke(cli, [])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_loglevel_invalid(self):
+        result = self.runner.invoke(cli, ['--log-level', 'foo'])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIn('invalid choice: foo', result.output)
+
+    def test_loglevel_override(self):
+        import logging
+        self.assertEqual(logging.getLogger('PIL').getEffectiveLevel(), logging.INFO)
+        result = self.runner.invoke(cli, ['--log-level', 'DEBUG'])
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(logging.getLogger('PIL').getEffectiveLevel(), logging.DEBUG)
+
+
+
+if __name__ == '__main__':
+    main()
