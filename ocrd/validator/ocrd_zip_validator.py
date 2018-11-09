82dab32853fdda927f304f6b575b424fbfcfe2d0
Konstantin Baierer <unixprog@gmail.com>
2018-11-09 16:49:15 +0100

    bagit: profile validation

--- ocrd/validator/ocrd_zip_validator.py
+++ ocrd/validator/ocrd_zip_validator.py
@@ -1,7 +1,11 @@
-from bagit_profile import Profile, ProfileValidationError
+from tempfile import mkdtemp
+from shutil import rmtree
 
-from ..constants import OCRD_BAGIT_PROFILE_URL
-from ..utils import getLogger
+from bagit import Bag
+from bagit_profile import Profile, ProfileValidationError # pylint: disable=no-name-in-module
+
+from ..constants import OCRD_BAGIT_PROFILE_URL, OCRD_BAGIT_PROFILE, TMP_BAGIT_PREFIX
+from ..utils import getLogger, unzip_file_to_dir
 from .report import ValidationReport
 
 log = getLogger('ocrd.ocrd_zip_validator')
@@ -20,25 +24,25 @@ class OcrdZipValidator(object):
         - https://ocr-d.github.io/bagit-profile.yml
     """
 
-    def __init__(self, resolver, bagzip):
+    def __init__(self, resolver, path_to_zip):
         """
         Arguments:
             resolver (Resolver): resolver
-            bagzip (string): Path to the OCRD-ZIP file
+            path_to_zip (string): Path to the OCRD-ZIP file
         """
         self.resolver = resolver
-        self.bagzip = bagzip
+        self.path_to_zip = path_to_zip
         self.report = ValidationReport()
-        self.profile_validator = Profile(OCRD_BAGIT_PROFILE_URL)
+        self.profile_validator = Profile(OCRD_BAGIT_PROFILE_URL, profile=OCRD_BAGIT_PROFILE)
 
     def validate(self):
         try:
-            self.profile_validator.validate_serialization(self.bagzip)
+            self.profile_validator.validate_serialization(self.path_to_zip)
         except (IOError, ProfileValidationError) as err:
             self.report.add_error(err.value)
-        #  self._validate_serialization()
-        #  self._ensure_bagdir
-        #  self._validate_mets_file_group_names()
-        #  self._validate_mets_files()
-        #  self._validate_pixel_density()
+        bagdir = mkdtemp(prefix=TMP_BAGIT_PREFIX)
+        unzip_file_to_dir(self.path_to_zip, bagdir)
+        bag = Bag(bagdir)
+        self.profile_validator.validate(bag)
+        rmtree(bagdir)
         return self.report
