8c11e30b62e50240d98b5dda8d07d69107643ef7
Konstantin Baierer <unixprog@gmail.com>
2018-11-20 17:01:32 +0100

    drop code for generating openapi defs

--- Makefile
+++ Makefile
@@ -86,7 +86,6 @@ repo/spec:
 .PHONY: spec
 # Copy JSON Schema, OpenAPI from OCR-D/spec
 spec: repo/spec
-	cp repo/spec/ocrd_api.swagger.yml ocrd/model/yaml/ocrd_oas3.spec.yml
 	cp repo/spec/ocrd_tool.schema.yml ocrd/model/yaml/ocrd_tool.schema.yml
 	cp repo/spec/bagit-profile.yml ocrd/model/yaml/bagit-profile.yml
 
--- ocrd/cli/__init__.py
+++ ocrd/cli/__init__.py
@@ -2,10 +2,7 @@ import click
 
 from ocrd.cli.ocrd_tool import ocrd_tool_cli
 from ocrd.cli.workspace import workspace_cli
-from ocrd.cli.generate_swagger import generate_swagger_cli
 from ocrd.cli.process import process_cli
-# TODO server CLI disabled
-#  from ocrd.cli.server import server_cli
 from ocrd.cli.bashlib import bashlib_cli
 from ocrd.decorators import ocrd_loglevel
 from .zip import zip_cli
@@ -20,9 +17,6 @@ def cli(**kwargs): # pylint: disable=unused-argument
 
 cli.add_command(ocrd_tool_cli)
 cli.add_command(workspace_cli)
-cli.add_command(generate_swagger_cli)
 cli.add_command(process_cli)
-# TODO server CLI disabled
-#  cli.add_command(server_cli)
 cli.add_command(bashlib_cli)
 cli.add_command(zip_cli)
