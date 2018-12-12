bf646b59638a4796c0c4819a7a967232d3380f0f
Konstantin Baierer <unixprog@gmail.com>
2018-12-12 13:50:41 +0100

    groupid_to_structmap: script to update legacy GROUPID to current structMap paging

---
+++ groupid_to_structmap.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from sys import argv
+from os.path import isfile
+from ocrd.model import OcrdMets
+
+fname = argv[1]
+if not isfile(fname):
+    raise "File not found %s" % fname
+mets = OcrdMets(filename=fname)
+
+# pylint: disable=protected-access
+for f in mets.find_files():
+    if not f.pageId:
+        groupid = f._el.get('GROUPID')
+        if groupid:
+            del f._el.attrib['GROUPID']
+        else:
+            groupid = "FIXME"
+            print("!! File %s has neither GROUPID nor mets:fptr in the PHYSICAL structMap" % f.url)
+        print("Setting page of %s to %s" % (f.ID, groupid))
+        f.pageId = groupid
+
+with open(fname, 'wb') as out:
+    out.write(mets.to_xml(xmllint=True))
