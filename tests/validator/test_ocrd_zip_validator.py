00724e12661fc1c4a3894c939ecdb0d4778aca32
Konstantin Baierer <unixprog@gmail.com>
2019-02-19 12:03:00 +0100

    test zip validator

--- tests/validator/test_ocrd_zip_validator.py
+++ tests/validator/test_ocrd_zip_validator.py
@@ -1,6 +1,9 @@
-from tests.base import TestCase, main, assets # pylint: disable=import-error,no-name-in-module
-from ocrd_validators import OcrdZipValidator
+from os import unlink
+from os.path import join
 
+from tests.base import TestCase, main, assets # pylint: disable=import-error,no-name-in-module
+
+from ocrd_validators import OcrdZipValidator
 from ocrd.workspace import Workspace
 from ocrd.workspace_bagger import WorkspaceBagger
 from ocrd.resolver import Resolver
@@ -24,6 +27,15 @@ class TestOcrdZipValidator(TestCase):
         report = validator.validate(skip_unzip=True)
         self.assertEqual(report.is_valid, True)
         print(report)
+
+    def test_fail_validation_unzipped_extra_files(self):
+        validator = OcrdZipValidator(self.resolver, assets.path_to('kant_aufklaerung_1784'))
+        extrapath = join(assets.path_to('kant_aufklaerung_1784'), 'data', 'EXTRA')
+        with open(extrapath, 'w') as f:
+            f.write('FAIL')
+        with self.assertRaisesRegex(Exception, "Payload-Oxum validation failed"):
+            validator.validate(skip_unzip=True)
+        unlink(extrapath)
 
 if __name__ == '__main__':
     main()
