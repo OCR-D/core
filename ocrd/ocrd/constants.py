09ff565205852ac3e64f6dd4fe3c3c8ff4bf1f3e
bertsky <sachunsky@informatik.uni-leipzig.de>
2019-07-17 09:14:26 +0200

    bashlib: now resides in root of pkg

--- ocrd/ocrd/constants.py
+++ ocrd/ocrd/constants.py
@@ -7,5 +7,5 @@ TMP_PREFIX = 'pyocrd-'
 DEFAULT_UPLOAD_FOLDER = '/tmp/uploads-pyocrd'
 DOWNLOAD_DIR = '/tmp/ocrd-core-downloads'
 DEFAULT_REPOSITORY_URL = 'http://localhost:5000/'
-BASHLIB_FILENAME = resource_filename(__name__, '../lib.bash')
+BASHLIB_FILENAME = resource_filename(__name__, 'lib.bash')
 BACKUP_DIR = '.backup'
