a4e20ac29c6515b437b54880072ba735e39255c8
Konstantin Baierer <unixprog@gmail.com>
2019-09-03 17:35:42 +0200

    model_factory: check whether file exists before trying to parse as PAGE, fix #302

--- ocrd_modelfactory/ocrd_modelfactory/__init__.py
+++ ocrd_modelfactory/ocrd_modelfactory/__init__.py
@@ -4,6 +4,7 @@ Factory methods to create models for data, files, URLs.
 
 """
 from datetime import datetime
+from pathlib import Path
 
 from PIL import Image
 
@@ -39,8 +40,10 @@ def page_from_image(input_file):
     Arguments:
         * input_file (OcrdFile):
     """
-    if input_file.local_filename is None:
-        raise Exception("input_file must have 'local_filename' property")
+    if not input_file.local_filename:
+        raise ValueError("input_file must have 'local_filename' property")
+    if not Path(input_file.local_filename).exists():
+        raise FileNotFoundError("File not found: '%s' (%s)" % (input_file.local_filename, input_file))
     exif = exif_from_filename(input_file.local_filename)
     now = datetime.now()
     return PcGtsType(
@@ -64,11 +67,12 @@ def page_from_file(input_file):
     Arguments:
         * input_file (OcrdFile):
     """
-    #  from os import getcwd
-    #  from os.path import exists
-    #  print(">>>>>> cwd='%s' local_filename=%s exists=%s" %(getcwd(), input_file.local_filename, exists(input_file.local_filename)))
+    if not input_file.local_filename:
+        raise ValueError("input_file must have 'local_filename' property")
+    if not Path(input_file.local_filename).exists():
+        raise FileNotFoundError("File not found: '%s' (%s)" % (input_file.local_filename, input_file))
     if input_file.mimetype.startswith('image'):
         return page_from_image(input_file)
     if input_file.mimetype == MIMETYPE_PAGE:
         return parse(input_file.local_filename, silence=True)
-    raise Exception("Unsupported mimetype '%s'" % input_file.mimetype)
+    raise ValueError("Unsupported mimetype '%s'" % input_file.mimetype)
