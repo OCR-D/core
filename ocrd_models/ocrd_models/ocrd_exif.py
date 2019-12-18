f4726afc41f47526a889fcd1af270a6edb289b04
Konstantin Baierer <unixprog@gmail.com>
2019-12-18 17:34:57 +0100

    OcrdExif: Catch jfif_density not being set for JPEG files

--- ocrd_models/ocrd_models/ocrd_exif.py
+++ ocrd_models/ocrd_models/ocrd_exif.py
@@ -40,7 +40,7 @@ class OcrdExif():
             self.xResolution = int(img.info['dpi'][0])
             self.yResolution = int(img.info['dpi'][1])
             self.resolutionUnit = 'cm' if img.tag.get(296) == 3 else 'inches'
-        elif img.format == 'JPEG':
+        elif img.format == 'JPEG' and 'jfif_density' in img.info:
             self.xResolution = img.info['jfif_density'][0]
             self.yResolution = img.info['jfif_density'][1]
             self.resolutionUnit = 'cm' if img.info['jfif_unit'] == 2 else 'inches'
