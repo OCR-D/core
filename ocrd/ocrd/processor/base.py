6972e6897cbab0b6df33a7e4b8c5cd9604c47fde
Konstantin Baierer <kba@users.noreply.github.com>
2020-01-17 11:55:08 +0100

    Update ocrd/ocrd/processor/base.py
    Co-Authored-By: Robert Sachunsky <38561704+bertsky@users.noreply.github.com>

--- ocrd/ocrd/processor/base.py
+++ ocrd/ocrd/processor/base.py
@@ -105,7 +105,8 @@ def generate_processor_help(ocrd_tool):
             parameter_help += wrap_text('  "%s" [%s%s] %s' % (
                 param_name,
                 param['type'],
-                ' - REQUIRED' if 'required' in param and param['required'] else '',
+                ' - REQUIRED' if 'required' in param and param['required'] else
+                ' - %s' % param['default'] if 'default' in param else '',
                 ' - %s' % param['default'] if 'default' in param else '',
                 param['description']
             ), subsequent_indent='    ', width=72, preserve_paragraphs=True)
