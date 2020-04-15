e93ca1e911da925bd094427b68a9c199b8c4f0bf
Uwe Hartwig <uwe.hartwig@bitsrc.info>
2020-04-15 14:06:34 +0200

    Update ocrd_utils/ocrd_utils/logging.py
    Co-Authored-By: Robert Sachunsky <38561704+bertsky@users.noreply.github.com>

--- ocrd_utils/ocrd_utils/logging.py
+++ ocrd_utils/ocrd_utils/logging.py
@@ -97,7 +97,11 @@ def initLogging():
         level=logging.INFO,
         format='%(asctime)s.%(msecs)03d %(levelname)s %(name)s - %(message)s',
         datefmt='%H:%M:%S')
+    #  logging.getLogger('ocrd.resolver').setLevel(logging.INFO)
+    #  logging.getLogger('ocrd.resolver.download_to_directory').setLevel(logging.INFO)
+    #  logging.getLogger('ocrd.resolver.add_files_to_mets').setLevel(logging.INFO)
     logging.getLogger('PIL').setLevel(logging.INFO)
+    # To cut back on the `Self-intersection at or near point` INFO messages
     logging.getLogger('shapely.geos').setLevel(logging.ERROR)
     
 
