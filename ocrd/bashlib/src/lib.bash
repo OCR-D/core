ed395cc233e2fcc4f1e83fc0aefe84f683ff64ac
Konstantin Baierer <unixprog@gmail.com>
2020-04-23 21:42:17 +0200

    bashlib: add logging methods, #366

--- ocrd/bashlib/src/lib.bash
+++ ocrd/bashlib/src/lib.bash
@@ -1,4 +1,4 @@
-# HERE-INCLUDE ./src/raise.bash
+# HERE-INCLUDE ./src/logging.bash
 # HERE-INCLUDE ./src/dumpjson.bash
 # HERE-INCLUDE ./src/usage.bash
 # HERE-INCLUDE ./src/parse_argv.bash
--- ocrd/ocrd/lib.bash
+++ ocrd/ocrd/lib.bash
@@ -1,10 +1,39 @@
-# BEGIN-INCLUDE ./src/raise.bash 
+# BEGIN-INCLUDE ./src/logging.bash 
 ## ### `ocrd__raise`
 ## 
 ## Raise an error and exit.
 ocrd__raise () {
     echo >&2 "ERROR: $1"; exit 127
 }
+
+## ### `ocrd__log`
+## 
+## Log an error message.
+## Arguments: [`loglevel`, `message`] or just [`message`] (default level DEBUG)
+ocrd__scriptname="${0##*/}"
+ocrd__log () {
+    local loglevel timestamp;
+    if [ $# -gt 1 ];then
+        loglevel=$1 ; shift
+    else
+        loglevel='DEBUG'
+    fi
+    timestamp="$(date +'%T.%3N')"
+    echo >&2 "$timestamp $loglevel $ocrd__scriptname - $*"
+}
+
+## ### `ocrd__error`
+## ### `ocrd__warning`
+## ### `ocrd__info`
+## ### `ocrd__debug`
+## ### `ocrd__trace`
+## 
+## Log an error/warning/info/debug/trace message.
+ocrd__error () { ocrd__log ERROR "$1"; }
+ocrd__warning () { ocrd__log WARNING "$1"; }
+ocrd__info () { ocrd__log INFO "$1"; }
+ocrd__debug () { ocrd__log DEBUG "$1"; }
+ocrd__trace () { ocrd__log TRACE "$1"; }
 
 # END-INCLUDE 
 # BEGIN-INCLUDE ./src/dumpjson.bash 
