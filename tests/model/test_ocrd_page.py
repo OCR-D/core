fbbd7be2d5226f003f68e35e92565baf5f55363e
Konstantin Baierer <unixprog@gmail.com>
2020-04-29 17:48:29 +0200

    improve xsd testcase

--- tests/model/test_ocrd_page.py
+++ tests/model/test_ocrd_page.py
@@ -16,7 +16,7 @@ from ocrd_models.ocrd_page import (
 )
 
 simple_page = """\
-<PcGts xmlns="http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15 http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15/pagecontent.xsd">
+<PcGts xmlns="http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15 http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15/pagecontent.xsd">
     <Metadata>
         <Creator>OCR-D</Creator>
         <Created>2016-09-20T11:09:27.041+02:00</Created>
--- tests/validator/test_xsd_validator.py
+++ tests/validator/test_xsd_validator.py
@@ -2,8 +2,9 @@
 
 from tests.base import TestCase, main, assets
 from ocrd import Resolver
-from ocrd_validators import XsdValidator, XsdMetsValidator
-from ocrd_validators.constants import XSD_METS_URL
+from ocrd_validators import XsdValidator, XsdMetsValidator, XsdPageValidator
+from ocrd_validators.constants import XSD_METS_URL, XSD_PAGE_URL
+from tests.model.test_ocrd_page import simple_page
 
 class TestXsdValidator(TestCase):
 
@@ -30,26 +31,32 @@ class TestXsdValidator(TestCase):
         report = XsdValidator.validate(XSD_METS_URL, self.ws.mets._tree)
         self.assertTrue(report.is_valid)
 
-class TestXsdMetsValidator(TestXsdValidator):
+# class TestXsdMetsValidator(TestXsdValidator):
 
-    def test_validate_mets_simple_protected_doc(self):
-        val = XsdMetsValidator()
-        report = val._validate(self.ws.mets._tree)
-        self.assertTrue(report.is_valid)
+#     def test_validate_mets_simple_protected_doc(self):
+#         val = XsdMetsValidator()
+#         report = val._validate(self.ws.mets._tree)
+#         self.assertTrue(report.is_valid)
 
-    def test_validate_mets_simple_static_doc(self):
-        report = XsdValidator.validate(XSD_METS_URL, self.ws.mets._tree)
-        self.assertTrue(report.is_valid)
+#     def test_validate_mets_simple_static_doc(self):
+#         report = XsdMetsValidator.validate(self.ws.mets._tree)
+#         self.assertTrue(report.is_valid)
 
-class TestXsdPageValidator(TestXsdValidator):
+class TestXsdPageValidator(TestCase):
 
-    def test_validate_simple_protected_doc(self):
+    def setUp(self):
+        self.resolver = Resolver()
+        self.ws = self.resolver.workspace_from_url(assets.url_of('SBB0000F29300010000/data/mets.xml'))
+
+    def test_validate_page_simple_protected_doc(self):
         val = XsdPageValidator()
-        report = val._validate(self.ws.page._tree)
+        report = val._validate(simple_page)
+        print(report.errors)
         self.assertTrue(report.is_valid)
 
-    def test_validate_simple_static_doc(self):
-        report = XsdValidator.validate(XSD_PAGE_URL, self.ws.page._tree)
+    def test_validate_page_simple_static_doc(self):
+        report = XsdPageValidator.validate(simple_page)
+        print(report.errors)
         self.assertTrue(report.is_valid)
 
 if __name__ == '__main__':
