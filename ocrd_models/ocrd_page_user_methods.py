3445f87f027970d57227170115116cc440e624af
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2020-05-14 14:12:44 +0200

    Update ocrd_models/ocrd_page_user_methods.py
    Co-authored-by: Robert Sachunsky <38561704+bertsky@users.noreply.github.com>

--- ocrd_models/ocrd_page_user_methods.py
+++ ocrd_models/ocrd_page_user_methods.py
@@ -94,13 +94,32 @@ get_AllRegions = MethodSpec(name='get_AllRegions',
         Returned in random order unless ``reading_order`` is set (NOT CURRENTLY IMPLEMENTED)
         """
         if reading_order:
-            raise NotImplementedError("Ordering of regions by Reading Order not currently Implemented :(")
+            reading_order = self.get_ReadingOrder()
         if not regions:
-            regions = ['Advert', 'Chart', 'Chem', 'Custom', 'Graphic', 'Image', 'LineDrawing', 'Map', 'Maths', 'Music', 'Noise', 'Table', 'Text']
+            regions = ['Advert', 'Chart', 'Chem', 'Custom', 'Graphic', 'Image', 'LineDrawing', 'Map', 'Maths', 'Music', 'Noise', 'Separator', 'Table', 'Text', 'Unknown']
         ret = []
         for region in regions:
             ret += getattr(self, 'get_{}Region'.format(region))()
-        return ret
+        if reading_order:
+            reading_order = reading_order.get_OrderedGroup() or reading_order.get_UnorderedGroup()
+        if reading_order:
+            def get_recursive_reading_order(rogroup):
+                if isinstance(rogroup, (OrderedGroupType, OrderedGroupIndexedType)):
+                    elements = rogroup.get_AllIndexed()
+                if isinstance(rogroup, (UnorderedGroupType, UnorderedGroupIndexedType)):
+                    elements = (rogroup.get_RegionRef() + rogroup.get_OrderedGroup() + rogroup.get_UnorderedGroup())
+                regionrefs = list()
+                for elem in elements:
+                    regionrefs.append(elem.get_regionRef())
+                    if not isinstance(elem, (RegionRefType, RegionRefIndexedType)):
+                        regionrefs.extend(get_recursive_reading_order(elem))
+                return regionrefs
+            reading_order = get_recursive_reading_order(reading_order)
+        if reading_order:
+            ret = dict([(region.id, region) for region in ret])
+            return [ret[region_id] for region_id in reading_order if region_id in ret]
+        else:
+            return ret
     ''', class_names=r'^(PageType)$')
 
 #
