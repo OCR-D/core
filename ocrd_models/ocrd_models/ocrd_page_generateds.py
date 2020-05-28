ac62b857476967b44f7d81115424ca9c51fdbf93
kba <unixprog@gmail.com>
2020-05-28 15:59:05 +0200

    get_AllRegions: clean-up merge artifacts and reorganize

--- ocrd_models/ocrd_models/ocrd_page_generateds.py
+++ ocrd_models/ocrd_models/ocrd_page_generateds.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Generated Thu May 28 14:25:40 2020 by generateDS.py version 2.35.20.
+# Generated Thu May 28 15:57:42 2020 by generateDS.py version 2.35.20.
 # Python 3.6.9 (default, Apr 18 2020, 01:56:04)  [GCC 8.4.0]
 #
 # Command line options:
@@ -2850,58 +2850,68 @@ class PageType(GeneratedsSuper):
             obj_.original_tagname_ = 'CustomRegion'
     def __hash__(self):
         return hash(self.id)
-    def get_AllRegions(self, classes=None, order='document'):
+    # pylint: disable=line-too-long,invalid-name,protected-access,missing-module-docstring
+    def _region_class(self, x): # pylint: disable=unused-argument
+        return x.__class__.__name__.replace('RegionType', '')
+    
+    def _region_id(self, x): # pylint: disable=unused-argument
+        return x.id if hasattr(x, 'id') else x.pcGtsId
+    
+    def _get_recursive_regions(self, regions, level, classes=None):
+        if level == 1:
+            # stop recursion, filter classes
+            if classes:
+                return [r for r in regions if self._region_class(r) in classes]
+            # remove the first element (PageType)
+            return regions[1:]
+        # find more regions recursively
+        more_regions = []
+        for region in regions:
+            more_regions.append([])
+            for class_ in ['Advert', 'Chart', 'Chem', 'Custom', 'Graphic', 'Image',
+                           'LineDrawing', 'Map', 'Maths', 'Music', 'Noise',
+                           'Separator', 'Table', 'Text', 'Unknown']:
+                if class_ == 'Map' and not isinstance(region, PageType): # pylint: disable=undefined-variable
+                    # 'Map' is not recursive in 2019 schema
+                    continue
+                more_regions[-1] += getattr(region, 'get_{}Region'.format(class_))()
+        if not any(more_regions):
+            return _get_recursive_regions(regions, 1, classes)
+        regions = [region for r, more in zip(regions, more_regions) for region in [r] + more]
+        return self._get_recursive_regions(regions, level - 1 if level else 0, classes)
+    
+    def _get_recursive_reading_order(self, rogroup):
+        if isinstance(rogroup, (OrderedGroupType, OrderedGroupIndexedType)): # pylint: disable=undefined-variable
+            elements = rogroup.get_AllIndexed()
+        if isinstance(rogroup, (UnorderedGroupType, UnorderedGroupIndexedType)): # pylint: disable=undefined-variable
+            elements = (rogroup.get_RegionRef() + rogroup.get_OrderedGroup() + rogroup.get_UnorderedGroup())
+        regionrefs = list()
+        for elem in elements:
+            regionrefs.append(elem.get_regionRef())
+            if not isinstance(elem, (RegionRefType, RegionRefIndexedType)): # pylint: disable=undefined-variable
+                regionrefs.extend(self._get_recursive_reading_order(elem))
+        return regionrefs
+    
+    def get_AllRegions(self, classes=None, order='document', depth=1):
         """
         Get all the *Region element or only those provided by ``classes``.
         Returned in document order unless ``order`` is ``reading-order`` is set (NOT CURRENTLY IMPLEMENTED)
         Arguments:
-            classes (list) Classes of regions that shall be returned, e.g. ['Text', 'Image']
+            classes (list) Classes of regions that shall be returned, e.g. ``['Text', 'Image']``
             order ("document"|"reading-order") Whether to return regions sorted by document order (default) or by reading order
+            depth (int) Recursive depth to look for regions at. Default: 1
         """
         if order not in ['document', 'reading-order']:
             raise Exception("Argument 'order' must be either 'document' or 'reading-order', not '{}'".format(order))
-        def region_class(x):
-            return x.__class__.__name__.replace('RegionType', '')
-        def get_recursive_regions(regions, level):
-            if level == 1:
-                # stop recursion, filter classes
-                if classes:
-                    return [r for r in regions if region_class(r) in classes]
-                else:
-                    return regions
-            # find more regions recursively
-            more_regions = []
-            for region in regions:
-                more_regions.append([])
-                for class_ in ['Advert', 'Chart', 'Chem', 'Custom', 'Graphic', 'Image', 'LineDrawing', 'Map', 'Maths', 'Music', 'Noise', 'Separator', 'Table', 'Text', 'Unknown']:
-                    if class_ == 'Map' and not isinstance(region, PageType):
-                        # 'Map' is not recursive in 2019 schema
-                        continue
-                     more_regions[-1] += getattr(region, 'get_{}Region'.format(class_))()
-            if not any(more_regions):
-                return get_recursive_regions(regions, 1)
-            regions = [region for r, more in zip(regions, more_regions) for region in [r] + more]
-            return get_recursive_regions(regions, level - 1 if level else 0)
-        ret = get_recursive_regions([self], depth + 1 if depth else 0)
+        ret = self._get_recursive_regions([self], depth + 1 if depth else 0, classes)
         if order == 'reading-order':
             reading_order = self.get_ReadingOrder()
             if reading_order:
                 reading_order = reading_order.get_OrderedGroup() or reading_order.get_UnorderedGroup()
             if reading_order:
-                def get_recursive_reading_order(rogroup):
-                    if isinstance(rogroup, (OrderedGroupType, OrderedGroupIndexedType)):
-                        elements = rogroup.get_AllIndexed()
-                    if isinstance(rogroup, (UnorderedGroupType, UnorderedGroupIndexedType)):
-                        elements = (rogroup.get_RegionRef() + rogroup.get_OrderedGroup() + rogroup.get_UnorderedGroup())
-                    regionrefs = list()
-                    for elem in elements:
-                        regionrefs.append(elem.get_regionRef())
-                        if not isinstance(elem, (RegionRefType, RegionRefIndexedType)):
-                            regionrefs.extend(get_recursive_reading_order(elem))
-                    return regionrefs
-                reading_order = get_recursive_reading_order(reading_order)
+                reading_order = self._get_recursive_reading_order(reading_order)
             if reading_order:
-                id2region = dict([(region.id, region) for region in ret])
+                id2region = dict([(self._region_id(region), region) for region in ret]) # pylint: disable=consider-using-dict-comprehension
                 ret = [id2region[region_id] for region_id in reading_order if region_id in id2region]
         return ret
 # end class PageType
