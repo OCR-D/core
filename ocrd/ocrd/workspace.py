OCR-D/core

commit 5dce7269eb182d1f69899df8bab28ea41a33ebc4

Implement --overwrite with "overwrite_mode" on workspace

ocrd/ocrd/workspace.py
@@ -47,6 +47,7 @@ class Workspace():
         directory (string) : Folder to work in
         mets (:class:`OcrdMets`) : OcrdMets representing this workspace. Loaded from 'mets.xml' if ``None``.
         mets_basename (string) : Basename of the METS XML file. Default: Last URL segment of the mets_url.
+        overwrite_mode (boolean) : Whether to force add operations on this workspace globally
         baseurl (string) : Base URL to prefix to relative URL.
     """
 
@@ -54,6 +55,7 @@ class Workspace():
         self.resolver = resolver
         self.directory = directory
         self.mets_target = str(Path(directory, mets_basename))
+        self.overwrite_mode = False
         if mets is None:
             mets = OcrdMets(filename=self.mets_target)
         self.mets = mets
@@ -131,6 +133,8 @@ class Workspace():
             page_same_group (boolean): Remove only images in the same file group as the PAGE-XML. Has no effect unless ``page_recursive`` is ``True``.
         """
         log.debug('Deleting mets:file %s', ID)
+        if not force and self.overwrite_mode:
+            force = True
         if isinstance(ID, OcrdFile):
             ID = ID.ID
         try:
@@ -170,6 +174,8 @@ class Workspace():
             page_recursive (boolean): Whether to remove all images referenced in the file if the file is a PAGE-XML document.
             page_same_group (boolean): Remove only images in the same file group as the PAGE-XML. Has no effect unless ``page_recursive`` is ``True``.
         """
+        if not force and self.overwrite_mode:
+            force = True
         if USE not in self.mets.file_groups and not force:
             raise Exception("No such fileGrp: %s" % USE)
         if recursive:
@@ -807,6 +813,8 @@ class Workspace():
 
         Return the (absolute) path of the created file.
         """
+        if not force and self.overwrite_mode:
+            force = True
         image_bytes = io.BytesIO()
         image.save(image_bytes, format=MIME_TO_PIL[mimetype])
         file_path = str(Path(file_grp, '%s%s' % (file_id, MIME_TO_EXT[mimetype])))

ocrd/ocrd/decorators.py
@@ -33,7 +33,7 @@ def ocrd_cli_wrap_processor(
     mets=None,
     working_dir=None,
     dump_json=False,
-    help=False,
+    help=False, # pylint: disable=redefined-builtin
     version=False,
     overwrite=False,
     **kwargs
@@ -57,20 +57,24 @@ def ocrd_cli_wrap_processor(
         resolver = Resolver()
         workspace = resolver.workspace_from_url(mets, working_dir)
         page_id = kwargs.get('page_id')
+        # XXX not possible while processors do not adhere to # https://github.com/OCR-D/core/issues/505
+        # if overwrite:
+        #     if 'output_file_grp' not in kwargs or not kwargs['output_file_grp']:
+        #         raise Exception("--overwrite requires --output-file-grp")
+        #     LOG.debug("Removing files because of --overwrite")
+        #     for grp in kwargs['output_file_grp'].split(','):
+        #         if page_id:
+        #             for one_page_id in kwargs['page_id'].split(','):
+        #                 LOG.debug("Removing files in output file group %s with page ID %s", grp, one_page_id)
+        #                 for file in workspace.mets.find_files(pageId=one_page_id, fileGrp=grp):
+        #                     workspace.remove_file(file, force=True, keep_file=False, page_recursive=True)
+        #         else:
+        #             LOG.debug("Removing all files in output file group %s ", grp)
+        #             workspace.remove_file_group(grp, recursive=True, force=True, keep_files=False, page_recursive=True, page_same_group=True)
+        # XXX While https://github.com/OCR-D/core/issues/505 is open, set 'overwrite_mode' globally on the workspace
         if overwrite:
-            if 'output_file_grp' not in kwargs or not kwargs['output_file_grp']:
-                raise Exception("--overwrite requires --output-file-grp")
-            LOG.debug("Removing files because of --overwrite")
-            for grp in kwargs['output_file_grp'].split(','):
-                if page_id:
-                    for one_page_id in kwargs['page_id'].split(','):
-                        LOG.debug("Removing files in output file group %s with page ID %s", grp, one_page_id)
-                        for file in workspace.mets.find_files(pageId=one_page_id, fileGrp=grp):
-                            workspace.remove_file(file, force=True, keep_file=False, page_recursive=True)
-                else:
-                    LOG.debug("Removing all files in output file group %s ", grp)
-                    workspace.remove_file_group(grp, recursive=True, force=True, keep_files=False, page_recursive=True, page_same_group=True)
-        report = WorkspaceValidator.check_file_grp(workspace, kwargs['input_file_grp'], kwargs['output_file_grp'], page_id)
+            workspace.overwrite_mode = True
+        report = WorkspaceValidator.check_file_grp(workspace, kwargs['input_file_grp'], '' if overwrite else kwargs['output_file_grp'], page_id)
         if not report.is_valid:
             raise Exception("Invalid input/output file grps:\n\t%s" % '\n\t'.join(report.errors))
         run_processor(processorClass, ocrd_tool, mets, workspace=workspace, **kwargs)

tests/test_decorators.py
@@ -120,48 +120,60 @@ class TestDecorators(TestCase):
                     input_file_grp='IN-GRP',
                     output_file_grp='OUT-GRP',
                 )
-
-    def test_overwrite_group(self):
-        with self._sample_ws_for_overwrite() as ws:
-            self.assertTrue(exists(join(ws.directory, 'ID1.tif')), 'files exist')
-            self.assertTrue(exists(join(ws.directory, 'ID2.tif')), 'files exist')
-            self.assertTrue(exists(join(ws.directory, 'ID3.tif')), 'files exist')
-            self.assertTrue(exists(join(ws.directory, 'ID4.tif')), 'files exist')
+            # with overwrite, it shouldn't fail
             ocrd_cli_wrap_processor(
                 DummyProcessor,
                 ocrd_tool=DUMMY_TOOL,
                 mets=ws.mets_target,
-                parameter={"foo": 42},
                 input_file_grp='IN-GRP',
                 output_file_grp='OUT-GRP',
-                overwrite=True,
+                overwrite=True
             )
-            self.assertTrue(exists(join(ws.directory, 'ID1.tif')), 'files exist')
-            self.assertFalse(exists(join(ws.directory, 'ID2.tif')), 'files deleted')
-            self.assertFalse(exists(join(ws.directory, 'ID3.tif')), 'files deleted')
-            self.assertFalse(exists(join(ws.directory, 'ID4.tif')), 'files deleted')
 
-    def test_overwrite_group_page_id(self):
-        with self._sample_ws_for_overwrite() as ws:
-            self.assertTrue(exists(join(ws.directory, 'ID1.tif')), 'files exist')
-            self.assertTrue(exists(join(ws.directory, 'ID2.tif')), 'files exist')
-            self.assertTrue(exists(join(ws.directory, 'ID3.tif')), 'files exist')
-            self.assertTrue(exists(join(ws.directory, 'ID4.tif')), 'files exist')
-            ocrd_cli_wrap_processor(
-                DummyProcessor,
-                ocrd_tool=DUMMY_TOOL,
-                mets=ws.mets_target,
-                parameter={"foo": 42},
-                input_file_grp='IN-GRP',
-                output_file_grp='OUT-GRP',
-                overwrite=True,
-                page_id='pID2,pID4'
-            )
-            self.assertTrue(exists(join(ws.directory, 'ID1.tif')), 'files exist')
-            self.assertFalse(exists(join(ws.directory, 'ID2.tif')), 'files deleted')
-            self.assertTrue(exists(join(ws.directory, 'ID3.tif')), 'files exist')
-            self.assertFalse(exists(join(ws.directory, 'ID4.tif')), 'files deleted')
+    # XXX We cannot currently pre-emptively delete files because #505
+    # def test_overwrite_group(self):
+    #     with self._sample_ws_for_overwrite() as ws:
+    #         self.assertTrue(exists(join(ws.directory, 'ID1.tif')), 'files exist')
+    #         self.assertTrue(exists(join(ws.directory, 'ID2.tif')), 'files exist')
+    #         self.assertTrue(exists(join(ws.directory, 'ID3.tif')), 'files exist')
+    #         self.assertTrue(exists(join(ws.directory, 'ID4.tif')), 'files exist')
+    #         ocrd_cli_wrap_processor(
+    #             DummyProcessor,
+    #             ocrd_tool=DUMMY_TOOL,
+    #             mets=ws.mets_target,
+    #             parameter={"foo": 42},
+    #             input_file_grp='IN-GRP',
+    #             output_file_grp='OUT-GRP',
+    #             overwrite=True,
+    #         )
+    #         self.assertTrue(exists(join(ws.directory, 'ID1.tif')), 'files exist')
+    #         # self.assertFalse(exists(join(ws.directory, 'ID2.tif')), 'files deleted')
+    #         # self.assertFalse(exists(join(ws.directory, 'ID3.tif')), 'files deleted')
+    #         # self.assertFalse(exists(join(ws.directory, 'ID4.tif')), 'files deleted')
+
+    # XXX We cannot currently pre-emptively delete files because #505
+    # as it is the test therefore makes no sense
+    # def test_overwrite_group_page_id(self):
+    #     with self._sample_ws_for_overwrite() as ws:
+    #         self.assertTrue(exists(join(ws.directory, 'ID1.tif')), 'files exist')
+    #         self.assertTrue(exists(join(ws.directory, 'ID2.tif')), 'files exist')
+    #         self.assertTrue(exists(join(ws.directory, 'ID3.tif')), 'files exist')
+    #         self.assertTrue(exists(join(ws.directory, 'ID4.tif')), 'files exist')
+    #         ocrd_cli_wrap_processor(
+    #             DummyProcessor,
+    #             ocrd_tool=DUMMY_TOOL,
+    #             mets=ws.mets_target,
+    #             parameter={"foo": 42},
+    #             input_file_grp='IN-GRP',
+    #             output_file_grp='OUT-GRP',
+    #             overwrite=True,
+    #             page_id='pID2,pID4'
+    #         )
+    #         self.assertTrue(exists(join(ws.directory, 'ID1.tif')), 'files exist')
+    #         self.assertFalse(exists(join(ws.directory, 'ID2.tif')), 'files deleted')
+    #         self.assertTrue(exists(join(ws.directory, 'ID3.tif')), 'files exist')
+    #         self.assertFalse(exists(join(ws.directory, 'ID4.tif')), 'files deleted')
 
 
 if __name__ == '__main__':
-    main()
+    main(__file__)

commit 81606c55f1e3bfe65fa1fa4b4464c4a7a8c8f193

workspace.add_file: overwrite_mode → force=True
(regression from 5dce726)

ocrd/ocrd/workspace.py
@@ -206,6 +206,8 @@ class Workspace():
             content is not None)
         if content is not None and 'local_filename' not in kwargs:
             raise Exception("'content' was set but no 'local_filename'")
+        if self.overwrite_mode:
+            kwargs['force'] = True
 
         with pushd_popd(self.directory):
             if 'local_filename' in kwargs: