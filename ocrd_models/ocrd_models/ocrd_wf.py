b96f34f9f88ef439e7fd069cde10d8e8d0748e81
Konstantin Baierer <unixprog@gmail.com>
2020-08-15 19:08:30 +0200

    OcrdWf: start parsing algo, WIP

--- ocrd_models/ocrd_models/ocrd_wf.py
+++ ocrd_models/ocrd_models/ocrd_wf.py
@@ -1,3 +1,5 @@
+import re
+
 from .constants import OCRD_WF_SHEBANG
 
 class OcrdWf():
@@ -6,10 +8,35 @@ class OcrdWf():
         self.steps = steps if steps else []
         self.assignments = assignments if assignments else []
 
+    @staticmethod
+    def parse(src):
+        if src[0:len(OCRD_WF_SHEBANG)] != OCRD_WF_SHEBANG:
+            raise ValueError("OCRD-WF does not begin with '%s'!" % OCRD_WF_SHEBANG)
+        lines_wo_empty = []
+        # remove empty lines
+        for line in src.split("\n")[1:]:
+            if not re.fullmatch(r'^\s*$', line):
+                lines_wo_empty.append(line)
+        # strip comments
+        lines_wo_comment = []
+        for line in lines_wo_empty:
+            if not re.match("^\s*#", line):
+                lines_wo_comment.append(line)
+        lines_wo_continuation = []
+        # line continuation
+        n = 0
+        while n < len(lines_wo_comment)
+            continued_lines = 0
+            while lines_wo_comment[n].endswith('\\'):
+                continued_lines += 1
+                lines_wo_comment[n] = re.sub(r"\s*\\$", "", lines_wo_comment[n])
+                lines_wo_comment[n] += re.sub(r"^\s*", " ", lines_wo_comment[n + continued_lines])
+            n += 1 + continued_lines
+
     def __str__(self):
         ret = '%s\n' % OCRD_WF_SHEBANG
-        for kv in self.assignments:
-            k, v = kv
+        for k in self.assignments:
+            v = self.assignments[k]
             ret += '%s=%s\n' % (k, v)
         for step in self.steps:
             ret += '%s\n' % str(step)
