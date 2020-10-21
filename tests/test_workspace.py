898dfd47fc0c5ec9fbb26a9459eb199af85889d2
Konstantin Baierer <unixprog@gmail.com>
2020-10-21 13:49:05 +0200

    permissions test: no masking of file mode in assertion LHS, ht @stweil

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -286,7 +286,7 @@ class TestWorkspace(TestCase):
             mets_path = join(ws.directory, 'mets.xml')
             mask = umask(0)
             umask(mask)
-            assert (stat(mets_path).st_mode & ~mask) == 0o100664 & ~mask
+            assert (stat(mets_path).st_mode) == 0o100664 & ~mask
             chmod(mets_path, 0o777)
             ws.save_mets()
             assert filemode(stat(mets_path).st_mode) == '-rwxrwxrwx'
