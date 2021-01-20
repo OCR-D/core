a05ecf4d2812df1750b894d26760e4fea3c62d9b
Konstantin Baierer <unixprog@gmail.com>
2021-01-20 16:38:40 +0100

    fix ocrd_config test

--- tests/test_ocrd_config.py
+++ tests/test_ocrd_config.py
@@ -1,5 +1,6 @@
 from tests.base import main
 from unittest import mock
+from pathlib import Path
 
 import ocrd_utils
 
@@ -7,7 +8,8 @@ def test_config_loading():
     XDG_CONFIG_HOME_before = ocrd_utils.XDG_CONFIG_HOME
     with ocrd_utils.pushd_popd(tempdir=True) as tempdir:
         ocrd_utils.XDG_CONFIG_HOME = tempdir
-        with open('ocrd.yml', 'w', encoding='utf-8') as f:
+        Path('ocrd').mkdir()
+        with open('ocrd/config.yml', 'w', encoding='utf-8') as f:
             f.write('resource_location: cache\n')
         from ocrd.config import load_config_file
         obj = load_config_file()
