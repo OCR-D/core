cf1e13d8e42a05b2a19e03b1b8d6dbff063443b6
Konstantin Baierer <unixprog@gmail.com>
2021-02-27 17:35:50 +0100

    exif: log errors

--- ocrd_models/ocrd_models/ocrd_exif.py
+++ ocrd_models/ocrd_models/ocrd_exif.py
@@ -6,6 +6,7 @@ from math import sqrt
 from io import BytesIO
 from subprocess import run, PIPE
 from distutils.spawn import find_executable as which
+from ocrd_utils import getLogger
 
 class OcrdExif():
     """Represents technical image metadata.
@@ -35,25 +36,25 @@ class OcrdExif():
         self.height = img.height
         self.photometricInterpretation = img.mode
         self.n_frames = img.n_frames if 'n_frames' in img.__dict__ else 1
-        #  if img.format == 'PNG':
-        #      print(img.info)
         for prop in ['compression', 'photometric_interpretation']:
             setattr(self, prop, img.info[prop] if prop in img.info else None)
-        if img.format == 'JPEG2000':
-            # XXX find a way to get pixel density in jpeg2000
+        if img.filename:
+            ret = run(['identify', '-format', r'%x %y %U', img.filename], check=False, stderr=PIPE, stdout=PIPE)
+        else:
+            with BytesIO() as bio:
+                img.save(bio, format=img.format)
+                ret = run(['identify', '-format', r'%x %y %U', '/dev/stdin'], check=False, stderr=PIPE, stdout=PIPE, input=bio.getvalue())
+        if ret.returncode:
+            stderr = ret.stderr.decode('utf-8')
+            if not which('identify'):
+                raise Exception("The 'identify' command is not available. Install with 'sudo apt install imagemagick'")
+            if 'no decode delegate for this image format' in stderr:
+                getLogger('ocrd_exif').warning("ImageMagick does not support the '%s' image format. ", img.format)
+            else:
+                getLogger('ocrd_exif').error("identify exited with non-zero %s: %s", ret.returncode, stderr)
             self.xResolution = self.yResolution = 1
             self.resolutionUnit = 'inches'
         else:
-            if img.filename:
-                ret = run(['identify', '-format', r'%x %y %U', img.filename], check=False, stderr=PIPE, stdout=PIPE)
-            else:
-                with BytesIO() as bio:
-                    img.save(bio, format=img.format)
-                    ret = run(['identify', '-format', r'%x %y %U', '/dev/stdin'], check=False, stderr=PIPE, stdout=PIPE, input=bio.getvalue())
-            if ret.returncode:
-                if not which('identify'):
-                    raise Exception("The 'identify' command is not available. Install with 'sudo apt install imagemagick'")
-                raise ValueError("'identify' returned non-zero exit code %s. stderr: %s" % (ret.returncode, ret.stderr))
             tokens = ret.stdout.decode('utf-8').split(' ', 3)
             self.xResolution = int(float(tokens[0]))
             self.yResolution = int(float(tokens[1]))
