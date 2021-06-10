db14b507df16070d14355f4675d0a1eb0a91dbd5
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2021-06-11 01:20:54 +0200

    workflow server: run multi-processed / queued…
    - replace Flask dev server with external uwsgi call
    - factor out Flask app code into separate Python module
      which uWSGI can pick up
    - make uWSGI run given number of workers via multi-processing
      but not multi-threading, and prefork before loading app
      (to protect GPU and non-thread-safe processors, and because of GIL)
    - pass tasks and other settings via CLI options (wrapped in JSON)
    - set worker Harakiri (reload after timeout) based on number of
      pages multiplied by given page timeout
    - add option for number of processes and page timeout

---
+++ ocrd/ocrd/cli/server.py
@@ -0,0 +1,87 @@
+"""
+Flask application for uwsgi workflow server
+
+(This is not meant to be imported directly, but loaded from uwsgi.)
+"""
+import os
+import signal
+import json
+import flask
+import uwsgi # added to module path by uwsgi runner
+
+from ocrd_utils import getLogger, initLogging
+from ocrd.task_sequence import run_tasks, parse_tasks
+from ocrd.resolver import Resolver
+
+initLogging()
+# unwrap user-defined workflow:
+tasks = json.loads(uwsgi.opt["tasks"])
+loglevel = uwsgi.opt["loglevel"].decode()
+timeout_per_page = int(uwsgi.opt["timeout_per_page"])
+res = Resolver()
+app = flask.Flask(__name__)
+log = getLogger('ocrd.workflow.server')
+if loglevel:
+    log.setLevel(loglevel)
+
+def setup():
+    global tasks
+    log.info("Parsing and instantiating %d tasks", len(tasks))
+    tasks = parse_tasks(tasks) # raises exception if invalid (causing worker to exit)
+    for task in tasks:
+        task.instantiate() # returns False if impossible (causing CLI fallback below)
+
+@app.route('/process')
+def process(): # pylint: disable=unused-variable
+    log.debug("Processing request: %s", str(flask.request))
+    if flask.request.args.get("mets"):
+        mets = flask.request.args["mets"]
+    else:
+        return 'Error: No METS', 400
+    if flask.request.args.get('page_id'):
+        page_id = flask.request.args["page_id"]
+    else:
+        page_id = ''
+    if flask.request.args.get('log_level'):
+        log_level = flask.request.args["log_level"]
+    else:
+        log_level = None
+    if flask.request.args.get('overwrite'):
+        overwrite = flask.request.args["overwrite"] in ["True", "true", "1"]
+    else:
+        overwrite = False
+    try:
+        if page_id:
+            npages = len(page_id.split(','))
+        else:
+            workspace = res.workspace_from_url(mets)
+            npages = len(workspace.mets.physical_pages)
+        timeout = timeout_per_page * npages
+        log.info("Processing %d tasks on %d pages (timeout=%ds)", len(tasks), npages, timeout)
+        # FIXME: prevent multiple concurrent requests to the same workspace/METS
+        # (use internal routing rules to prevent that, perhaps send 503 or just push to backlog)
+        # allow no more than timeout_per_page before restarting worker:
+        uwsgi.set_user_harakiri(timeout) # go, go, go!
+        # run the workflow
+        run_tasks(mets, log_level, page_id, tasks, overwrite)
+        uwsgi.set_user_harakiri(0) # take a breath!
+    except Exception as e:
+        log.exception("Request '%s' failed", str(flask.request.args))
+        return 'Failed: %s' % str(e), 500
+    return 'Finished'
+
+@app.route('/list-tasks')
+def list_tasks(): # pylint: disable=unused-variable
+    seq = ''
+    for task in tasks:
+        seq += '\n' + str(task)
+    return seq
+@app.route('/shutdown')
+def shutdown(): # pylint: disable=unused-variable
+    log.debug("Shutting down")
+    # does not work ("error managing signal 2 on worker 1"):
+    # uwsgi.signal(signal.SIGINT)
+    os.kill(uwsgi.masterpid(), signal.SIGINT)
+    return 'Stopped'
+
+setup()
--- ocrd/ocrd/cli/workflow.py
+++ ocrd/ocrd/cli/workflow.py
@@ -2,12 +2,13 @@
 CLI for task_sequence
 """
 import sys
+import os
+import json
+import subprocess
 import click
-import flask
 import requests
 
 from ocrd_utils import getLogger, initLogging
-from ocrd.task_sequence import run_tasks, parse_tasks
 
 from ..decorators import ocrd_loglevel
 from .process import process_cli
@@ -41,23 +42,28 @@ def process_cli_alias(log_level, mets, page_id, tasks, overwrite):
 # ----------------------------------------------------------------------
 @workflow_cli.command('server')
 @ocrd_loglevel
+@click.option('-t', '--timeout', help="maximum processing time (in sec per page) before reloading worker (0 to disable)", default=0)
+@click.option('-j', '--processes', help="number of parallel workers to spawn", default=1)
 @click.option('-h', '--host', help="host name/IP to listen at", default='127.0.0.1')
 @click.option('-p', '--port', help="TCP port to listen at", default=5000, type=click.IntRange(min=1024))
 @click.argument('tasks', nargs=-1, required=True)
-def server_cli(log_level, host, port, tasks):
+def server_cli(log_level, timeout, processes, host, port, tasks):
     """
     Start server for a series of tasks to run processor CLIs or APIs on workspaces
 
     Parse the given tasks and try to instantiate all Pythonic
     processors among them with the given parameters.
-    Open a web server that listens on the given host and port
-    for GET requests named ``process`` with the following
+    Open a web server that listens on the given ``host`` and ``port``
+    and queues requests into ``processes`` worker processes
+    for GET requests named ``/process`` with the following
     (URL-encoded) arguments:
 
         mets (string): Path name (relative to the server's CWD,
                        or absolute) of the workspace to process
 
         page_id (string): Comma-separated list of page IDs to process
+
+        log_level (int): Override all logger levels during processing
 
         overwrite (bool): Remove output pages/images if they already exist
 
@@ -69,53 +75,38 @@ def server_cli(log_level, host, port, tasks):
     Also, between each contiguous chain of Pythonic tasks in the overall
     series, no METS de/serialization will be performed.
 
+    If processing does not finish before ``timeout`` seconds per page,
+    then the request will fail and the respective worker be reloaded.
+
+    To see the server's workflow configuration, send a GET request named
+    ``/list-tasks``.
+
     Stop the server by sending SIGINT (e.g. via ctrl+c
-    on the terminal), or sending a GET request named ``shutdown``.
+    on the terminal), or sending a GET request named ``/shutdown``.
     """
     log = getLogger('ocrd.workflow.server')
-    log.debug("Parsing and instantiating %d tasks", len(tasks))
-    tasks = parse_tasks(tasks)
-    for task in tasks:
-        task.instantiate()
-    app = flask.Flask(__name__)
-    @app.route('/process')
-    def process(): # pylint: disable=unused-variable
-        if flask.request.args.get("mets"):
-            mets = flask.request.args["mets"]
-        else:
-            return 'Error: No METS', 400
-        if flask.request.args.get('page_id'):
-            page_id = flask.request.args["page_id"]
-        else:
-            page_id = ''
-        if flask.request.args.get('overwrite'):
-            overwrite = flask.request.args["overwrite"] in ["True", "true", "1"]
-        else:
-            overwrite = False
-        try:
-            run_tasks(mets, log_level, page_id, tasks, overwrite)
-        except Exception as e:
-            log.exception("Request '%s' failed", str(flask.request.args))
-            return 'Failed: %s' % str(e), 500
-        return 'Finished'
-    @app.route('/list-tasks')
-    def list_tasks(): # pylint: disable=unused-variable
-        seq = ''
-        for task in tasks:
-            seq += '\n' + str(task)
-        return seq
-    @app.route('/shutdown')
-    def shutdown(): # pylint: disable=unused-variable
-        fun = flask.request.environ.get('werkzeug.server.shutdown')
-        if fun is None:
-            raise RuntimeError('Not running with the Werkzeug Server')
-        fun()
-        return 'Stopped'
-    log.debug("Running server on http://%s:%d", host, port)
-    # disable multithreading here:
-    # - GPU processors need to have same thread context between startup and processing
-    # - we have no multiprocessing server backend anyway (until we move to external server)
-    app.run(host=host, port=port, debug=False, threaded=False)
+    log.debug("Running server with %d workers on http://%s:%d", processes, host, port)
+    result = subprocess.run(["uwsgi", "--http-socket", "%s:%d" % (host, port),
+                             "--wsgi-file", os.path.join(os.path.dirname(__file__), 'server.py'),
+                             "--callable", "app", "--need-app",
+                             "--disable-logging", # OCR-D logging is enough
+                             # "--http-keepalive", "true",
+                             # "--add-header", "Connection: Keep-Alive",
+                             "--processes", "%d" % processes,
+                             "--master", "--single-interpreter",
+                             # "--daemonize2", # return from workflow CLI
+                             "--lazy-apps", # fork before loading app
+                             "--no-orphans", # kill workers if master dies
+                             "--die-on-term", # do not reload on SIGTERM
+                             "--reload-on-exception", # reload failed workers
+                             "--enable-threads", # for multithreading in Numpy, TF, ...
+                             # wrap in JSON to retain list/quotes (not possible with pyargv):
+                             "--set", "tasks=%s" % json.dumps(tasks),
+                             # server log level:
+                             "--set", "loglevel=%s" % (log_level or ''),
+                             # worker timeout per page:
+                             "--set", "timeout_per_page=%d" % timeout])
+    return result.returncode
 
 # ----------------------------------------------------------------------
 # ocrd workflow client
@@ -134,17 +125,19 @@ def client_cli(ctx, host, port):
     ctx.obj['log'] = getLogger('ocrd.workflow.client')
 
 @client_cli.command('process')
+@ocrd_loglevel
 @click.option('-m', '--mets', help="METS to process", default="mets.xml")
 @click.option('-g', '--page-id', help="ID(s) of the pages to process")
 @click.option('--overwrite', is_flag=True, default=False, help="Remove output pages/images if they already exist")
 @click.pass_context
-def client_process_cli(ctx, mets, page_id, overwrite):
+def client_process_cli(ctx, log_level, mets, page_id, overwrite):
     """
     Have the workflow server process another workspace
     """
     url = ctx.obj['URL'] + 'process'
     params = {'mets': mets,
               'page_id': page_id,
+              'log_level': log_level,
               'overwrite': str(overwrite)
     }
     try:
