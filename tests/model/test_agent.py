14141eb92f47e7b5e929efcaa127f59265c087ac
Uwe Hartwig <uwe.hartwig@bitsrc.info>
2021-12-05 22:57:45 +0100

    [test][rfct] exchange unittest with pytest

--- tests/model/test_agent.py
+++ tests/model/test_agent.py
@@ -1,32 +1,34 @@
-from tests.base import TestCase, assets, main # pylint: disable=unused-import
+# -*- coding: utf-8 -*-
 
 from ocrd_models import OcrdAgent
 
-# pylint: disable=no-member
-class TestOcrdAgent(TestCase):
 
-    def test_basic1(self):
-        ag = OcrdAgent(role='FOO')
-        self.assertEqual(ag.role, 'FOO')
-        self.assertEqual(ag.name, None)
-        self.assertEqual(str(ag), '<OcrdAgent [type=---, othertype=---, role=FOO, otherrole=---, name=---]/>')
+def test_init_role_no_name():
+    ag = OcrdAgent(role='FOO')
+    assert 'FOO' == ag.role
+    assert None == ag.name
 
-    def test_basic2(self):
-        ag = OcrdAgent(otherrole='BAR', othertype='x')
-        self.assertEqual(ag.role, 'OTHER')
-        self.assertEqual(ag.otherrole, 'BAR')
-        self.assertEqual(ag.othertype, 'x')
 
-    def test_basic3(self):
-        ag = OcrdAgent(name='foobar')
-        self.assertEqual(ag.name, 'foobar')
-        ag.name = 'barfoo'
-        self.assertEqual(ag.name, 'barfoo')
+def test_init_role_as_string():
+    ag = OcrdAgent(role='FOO')
+    expected = '<OcrdAgent [type=---, othertype=---, role=FOO, otherrole=---, name=---]/>'
+    assert expected == str(ag)
 
-    def test_basic4(self):
-        ag = OcrdAgent(othertype='foobar')
-        self.assertEqual(ag.type, 'OTHER')
-        #  print(ag)
 
-if __name__ == '__main__':
-    main()
+def test_init_otherrole_and_othertype():
+    ag = OcrdAgent(otherrole='BAR', othertype='x')
+    assert 'OTHER' == ag.role
+    assert 'BAR' == ag.otherrole
+    assert 'x' == ag.othertype
+
+
+def test_init_othertype():
+    ag = OcrdAgent(othertype='foobar')
+    assert 'OTHER' == ag.type
+
+
+def test_set_name():
+    ag = OcrdAgent(name='foobar')
+    assert 'foobar' == ag.name
+    ag.name = 'barfoo'
+    assert 'barfoo' == ag.name
