494b1926ec755e6665d3ce520ac0010ceaf89b84
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2022-05-13 00:01:29 +0200

    safe_filename: allow underscore, avoid dot

--- ocrd_utils/ocrd_utils/str.py
+++ ocrd_utils/ocrd_utils/str.py
@@ -175,7 +175,7 @@ def safe_filename(url):
     """
     Sanitize input to be safely used as the basename of a local file.
     """
-    ret = re.sub(r'[^A-Za-z0-9]+', '.', url)
+    ret = re.sub(r'[^A-Za-z0-9_]+', '_', url)
     ret = re.sub(r'^\.*', '', ret)
     ret = re.sub(r'\.\.*', '.', ret)
     #  print('safe filename: %s -> %s' % (url, ret))
