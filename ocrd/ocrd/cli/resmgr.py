OCR-D/core

commit 5a6ccf33ee250947b38105188d95f93b4f7c204b

implement foundation of ocrd resmgr

ocrd/ocrd/cli/resmgr.py
@@ -1,0 +1,43 @@
+import sys
+
+import click
+
+from ocrd_utils import initLogging
+from ocrd_validators import OcrdZipValidator
+
+from ..resource_manager import OcrdResourceManager
+
+@click.group("resmgr")
+def resmgr_cli():
+    """
+    Managing processor resources
+    """
+    initLogging()
+
+# ----------------------------------------------------------------------
+# ocrd zip list-available
+# ----------------------------------------------------------------------
+
+@resmgr_cli.command('list-available')
+@click.option('-e', '--executable', help='Show only resources for executable EXEC', metavar='EXEC')
+def list_available(executable=None):
+    """
+    List available resources
+    """
+    resmgr = OcrdResourceManager()
+    for executable, reslist in resmgr.list_available(executable):
+        print('%s' % executable)
+        for resdict in reslist:
+            print('- %s (%s)\n  %s' % (resdict['name'], resdict['url'], resdict['description']))
+        print()
+
+@resmgr_cli.command('list-installed')
+@click.option('-e', '--executable', help='Show only resources for executable EXEC', metavar='EXEC')
+def list_installed(executable=None):
+    """
+    List installed resources
+    """
+    resmgr = OcrdResourceManager()
+    ret = []
+    for executable, reslist in resmgr.list_installed(executable):
+        print(executable, reslist)

ocrd/ocrd/cli/__init__.py
@@ -18,6 +18,7 @@ from ocrd.cli.workspace import workspace_cli
 from ocrd.cli.process import process_cli
 from ocrd.cli.bashlib import bashlib_cli
 from ocrd.cli.validate import validate_cli
+from ocrd.cli.resmgr import resmgr_cli
 from ocrd.decorators import ocrd_loglevel
 from .zip import zip_cli
 from .log import log_cli
@@ -37,3 +38,4 @@ cli.add_command(bashlib_cli)
 cli.add_command(zip_cli)
 cli.add_command(validate_cli)
 cli.add_command(log_cli)
+cli.add_command(resmgr_cli)

ocrd/ocrd/constants.py
@@ -9,6 +9,7 @@ __all__ = [
     'DOWNLOAD_DIR',
     'DEFAULT_REPOSITORY_URL',
     'BASHLIB_FILENAME',
+    'RESOURCE_LIST_FILENAME',
     'BACKUP_DIR',
 ]
 
@@ -17,4 +18,5 @@ DEFAULT_UPLOAD_FOLDER = '/tmp/uploads-ocrd-core'
 DOWNLOAD_DIR = '/tmp/ocrd-core-downloads'
 DEFAULT_REPOSITORY_URL = 'http://localhost:5000/'
 BASHLIB_FILENAME = resource_filename(__name__, 'lib.bash')
+RESOURCE_LIST_FILENAME = resource_filename(__name__, 'resource_list.yml')
 BACKUP_DIR = '.backup'

ocrd/ocrd/resource_list.yml
@@ -1,0 +1,20 @@
+# List available resources by processor for "ocrd resmgr"
+ocrd-tesserocr-recognize:
+  - url: https://ub-backup.bib.uni-mannheim.de/~stweil/ocrd-train/data/Fraktur_5000000/tessdata_fast/Fraktur_50000000.334_450937.traineddata
+    type: file
+    name: Fraktur_GT4HistOCR.traineddata
+    description: Tesseract LSTM model trained on GT4HistOCR
+    version_range: '>= 0.0.1'
+ocrd-calamari-recognize:
+  - url: https://qurator-data.de/calamari-models/GT4HistOCR/2019-07-22T15_49+0200/model.tar.xz
+    type: archive
+    name: qurator-gt4hist-0.3
+    description: Calamari model trained with GT4HistOCR
+    path_in_archive: '.'
+    version_range: '< 1.0.0'
+  - url: https://qurator-data.de/calamari-models/GT4HistOCR/2019-12-11T11_10+0100/model.tar.xz
+    type: archive
+    name: qurator-gt4hist-1.0
+    description: Calamari model trained with GT4HistOCR
+    path_in_archive: '.'
+    version_range: '>= 1.0.0'

ocrd/ocrd/resource_manager.py
@@ -1,0 +1,58 @@
+from pathlib import Path
+
+from yaml import safe_load
+
+from .constants import RESOURCE_LIST_FILENAME
+
+from ocrd_validators import OcrdResourceListValidator
+from ocrd_utils import getLogger
+from ocrd_utils.constants import HOME
+from ocrd_utils.os import list_resource_candidates, list_all_resources
+
+builtin_list_filename = Path(RESOURCE_LIST_FILENAME)
+user_list_filename = Path(HOME, 'ocrd', 'resources.yml')
+
+class OcrdResourceManager():
+
+    """
+    Managing processor resources
+    """
+    def __init__(self):
+        self.log = getLogger('ocrd.resource_manager')
+        self.database = {}
+        self.load_resource_list(builtin_list_filename)
+        self.load_resource_list(user_list_filename)
+
+    def load_resource_list(self, list_filename):
+        if list_filename.is_file():
+            with open(list_filename, 'r', encoding='utf-8') as f:
+                list_loaded = safe_load(f)
+            report = OcrdResourceListValidator.validate(list_loaded)
+            if not report.is_valid:
+                self.log.error('\n'.join(report.errors))
+                raise ValueError("Resource list %s is invalid!" % (list_filename))
+            for executable, resource_list in list_loaded.items():
+                if executable not in self.database:
+                    self.database[executable] = []
+                # Prepend, so user provided is sorted before builtin
+                self.database[executable] = list_loaded[executable] + self.database[executable]
+
+    def list_available(self, executable=None):
+        if executable:
+            resources = [(executable, self.database[executable])]
+        else:
+            resources = [(x, y) for x, y in self.database.items()]
+        return resources
+
+    def list_installed(self, executable=None):
+        ret = []
+        for executable in [executable] if executable else self.database.keys():
+            reslist = []
+            for res_filename in list_all_resources(executable):
+                res_name = Path(res_filename).name
+                resdict = [x for x in self.database[executable] if x['name'] == res_name]
+                if not resdict:
+                    resdict = [{'name': res_name, 'url': '???', 'description': '???', 'version_range': '???'}]
+                reslist.append(resdict[0])
+            ret.append((executable, reslist))
+        return ret

ocrd_validators/ocrd_validators/resource_list.schema.yml
@@ -8,6 +8,7 @@ patternProperties:
       additionalProperties: false
       required:
         - url
+        - description
         - type
         - name
         - version_range
@@ -15,16 +16,19 @@ patternProperties:
         url:
           type: string
           description: URLs of all components of this resource
+        description:
+          type: string
+          description: A description of the resource
         name:
           type: string
           description: Name to store the resource as
         type:
           type: string
-          enum: ['direct-link', 'github-file', 'github-dir', 'tarball-link', 'zip-link']
+          enum: ['file', 'github-file', 'github-dir', 'archive']
           description: Type of the URL
         path_in_archive:
           type: string
-          description: if type is tarball-link or zip-link, the resource is at this location in the archive
+          description: if type is archive, the resource is at this location in the archive
         version_range:
           type: string
           description: Range of supported versions, syntax like in PEP 440

commit 75ae07d155e35cf0fea58c70c9e28494c5f08dd4

Remove unused local variable
This fixes an alert from LGTM.

Fixes: 5a6ccf33ee250947 ("implement foundation of ocrd resmgr")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

ocrd/ocrd/cli/resmgr.py
@@ -55,7 +55,6 @@ def list_installed(executable=None):
     List installed resources
     """
     resmgr = OcrdResourceManager()
-    ret = []
     for executable, reslist in resmgr.list_installed(executable):
         print_resources(executable, reslist, resmgr)
 