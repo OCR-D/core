8b583d9fd261fc047e69a55204b7f0025d59026e
joschrew <jonas.schrewe@gwdg.de>
2023-02-09 17:55:14 +0100

    Add workspace model basically that from webapi

--- ocrd/ocrd/network/database.py
+++ ocrd/ocrd/network/database.py
@@ -2,8 +2,12 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from ocrd.network.models.job import Job
+from ocrd.network.models.workspace import Workspace
 
 
 async def initiate_database(db_url: str):
     client = AsyncIOMotorClient(db_url)
-    await init_beanie(database=client.get_default_database(default='ocrd'), document_models=[Job])
+    await init_beanie(
+        database=client.get_default_database(default='ocrd'),
+        document_models=[Job, Workspace]
+    )
---
+++ ocrd/ocrd/network/models/workspace.py
@@ -0,0 +1,26 @@
+from beanie import Document
+from typing import Optional
+
+
+class Workspace(Document):
+    """
+    Model to store a workspace in the mongo-database.
+
+    Information to handle workspaces and from bag-info.txt are stored here.
+
+    Attributes:
+        ocrd_identifier             Ocrd-Identifier (mandatory)
+        bagit_profile_identifier    BagIt-Profile-Identifier (mandatory)
+        ocrd_base_version_checksum  Ocrd-Base-Version-Checksum (mandatory)
+        ocrd_mets                   Ocrd-Mets (optional)
+        bag_info_adds               bag-info.txt can also (optionally) contain additional
+                                    key-value-pairs which are saved here
+    """
+    id: str
+    workspace_mets_path: str
+    ocrd_identifier: str
+    bagit_profile_identifier: str
+    ocrd_base_version_checksum: Optional[str]
+    ocrd_mets: Optional[str]
+    bag_info_adds: Optional[dict]
+    deleted: bool = False
