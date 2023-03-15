OCR-D/core

commit f1527a2f605f3ea5ec89180b934291418ef8693e

:fire: upgrade base image to 20.04

Dockerfile
@@ -19,11 +19,12 @@ COPY README.md .
 COPY LICENSE .
 RUN echo 'APT::Install-Recommends "0"; APT::Install-Suggests "0";' >/etc/apt/apt.conf.d/ocr-d.conf
 RUN apt-get update && apt-get -y install software-properties-common \
-    && add-apt-repository ppa:deadsnakes/ppa \
     && apt-get update && apt-get -y install \
         ca-certificates \
-        python3.7-dev \
-        python3.7-venv \
+        python3-dev \
+        python3-pip \
+        python3-venv \
+        gcc \
         make \
         wget \
         time \
@@ -31,9 +32,9 @@ RUN apt-get update && apt-get -y install software-properties-common \
         sudo \
         git \
     && make deps-ubuntu \
-    && python3.7 -m ensurepip --default-pip \
     && pip install --upgrade pip setuptools \
     && make install \
+    && apt-get remove -y gcc \
     && $FIXUP \
     && rm -rf /build-ocrd
 

Makefile
@@ -49,7 +49,7 @@ help:
 DOCKER_TAG = ocrd/core
 
 # Docker base image. Default: '$(DOCKER_BASE_IMAGE)'.
-DOCKER_BASE_IMAGE = ubuntu:18.04
+DOCKER_BASE_IMAGE = ubuntu:20.04
 
 # Additional arguments to docker build. Default: '$(DOCKER_ARGS)'
 DOCKER_ARGS = 

commit c03059ffe5f838e36df0fca97b3fec59ef4f8e33

Dockerfile: Remove unneeded installation of python3-pip
Fixes: f1527a2f605f3ea5 ("upgrade base image to 20.04")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

Dockerfile
@@ -22,7 +22,6 @@ RUN apt-get update && apt-get -y install software-properties-common \
     && apt-get update && apt-get -y install \
         ca-certificates \
         python3-dev \
-        python3-pip \
         python3-venv \
         gcc \
         make \