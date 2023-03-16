5ff8860a004a81f6e9fd9d5a93e51040bfc5766c
joschrew <jonas.schrewe@gwdg.de>
2023-03-16 09:35:33 +0100

    Add module docstring for database

--- ocrd_network/ocrd_network/database.py
+++ ocrd_network/ocrd_network/database.py
@@ -1,3 +1,17 @@
+""" The database is used to store information regarding jobs and workspaces.
+
+Jobs: for every process-request a job is inserted into the database with a uuid, status and
+information about the process like parameters and filegroups. It is mainly used to track the status
+(`ocrd_network.models.job.StateEnum`) of a job so that the state of a job can be queried. Finished
+jobs are not deleted from the database.
+
+Workspaces: A job or a processor always runs on a workspace. So a processor needs the information
+where the workspace is available. This information can be set with providing an absolute path or a
+workspace_id. With the latter, the database is used to convert the workspace_id to a path.
+
+XXX: Currently the information is not preserved after the processing-server shuts down as the
+database (runs in docker) currently has no volume set.
+"""
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
