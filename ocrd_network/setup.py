93b7f5b117c17f4765993e98c1b1d06d508988c3
mehmedGIT <mehmed.n.mustafa@gmail.com>
2023-03-22 18:08:50 +0100

    Refactor the setup.py

--- ocrd_network/setup.py
+++ ocrd_network/setup.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-
+from setuptools import setup
 from ocrd_utils import VERSION
 
 install_requires = open('requirements.txt').read().split('\n')
 install_requires.append('ocrd_utils == %s' % VERSION)
 install_requires.append('ocrd_validators == %s' % VERSION)
 
-# TODO: This needs to be revisited! Seems badly adapted from ocrd/setup.py
 setup(
     name='ocrd_network',
     version=VERSION,
-    description='OCR-D framework - web API',
+    description='OCR-D framework - network',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Konstantin Baierer',
     author_email='unixprog@gmail.com',
     url='https://github.com/OCR-D/core',
     license='Apache License 2.0',
-    packages=find_packages(exclude=('tests', 'docs')),
-    include_package_data=True,
     install_requires=install_requires,
+    packages=['ocrd_network'],
     package_data={
         '': ['*.yml', '*.xsd']
     },
