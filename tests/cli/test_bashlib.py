e2e4dd92892c1e8a301a735eb3123a78291c78aa
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2023-03-30 21:24:01 +0200

    test_bashlib: add cp processor

--- tests/cli/test_bashlib.py
+++ tests/cli/test_bashlib.py
@@ -2,7 +2,9 @@ from tests.base import CapturingTestCase as TestCase, main, assets, copy_of_dire
 
 from pkg_resources import parse_version
 import subprocess
+import tempfile
 import yaml
+import json
 import pytest
 
 from ocrd.cli.bashlib import bashlib_cli
@@ -14,8 +16,12 @@ from ocrd_models.constants import TAG_MODS_IDENTIFIER
 
 class TestBashlibCli(TestCase):
 
-    def invoke_bash(self, script):
-        result = subprocess.run('bash -c "source $(ocrd bashlib filename) && %s"' % script, 
+    def invoke_bash(self, script, *args):
+        # pattern input=script would not work with additional args
+        with tempfile.NamedTemporaryFile() as scriptfile:
+            scriptfile.write(script)
+            scriptfile.close()
+            result = subprocess.run(['bash', scriptfile.name] + args,
                                 universal_newlines=True,
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
@@ -60,22 +66,91 @@ class TestBashlibCli(TestCase):
         with copy_of_directory(assets.path_to('kant_aufklaerung_1784/data')) as wsdir:
             with pushd_popd(wsdir):
                 _, out, err = self.invoke_cli(bashlib_cli, ['input-files', '-I', 'OCR-D-IMG'])
-                assert "[url]='OCR-D-IMG/INPUT_0017.tif' [ID]='INPUT_0017' [mimetype]='image/tiff'"
-                       "[pageId]='PHYS_0017' [outputFileId]='OUTPUT_PHYS_0017'" in out
+                assert ("[url]='OCR-D-IMG/INPUT_0017.tif' [ID]='INPUT_0017' [mimetype]='image/tiff'"
+                        "[pageId]='PHYS_0017' [outputFileId]='OUTPUT_PHYS_0017'") in out
 
     def test_bashlib_defs(self):
-        exit_code, out, err = self.invoke_bash("type -t ocrd__wrap && type -t ocrd__minversion")
+        exit_code, out, err = self.invoke_bash(
+            "source $(ocrd bashlib filename) && type -t ocrd__wrap && type -t ocrd__minversion")
         assert exit_code == 0
         assert len(err) == 0
         assert 'function' in out
 
     def test_bashlib_minversion(self):
-        exit_code, out, err = self.invoke_bash("ocrd__minversion 2.29.0")
+        exit_code, out, err = self.invoke_bash(
+            "source $(ocrd bashlib filename) && ocrd__minversion 2.29.0")
         assert exit_code == 0
         version = parse_version(VERSION)
-        exit_code, out, err = self.invoke_bash("ocrd__minversion %d.%d.%d" % (version.major, version.minor+1, 0))
+        version = "%d.%d.%d" % (version.major, version.minor+1, 0)
+        exit_code, out, err = self.invoke_bash(
+            "source $(ocrd bashlib filename) && ocrd__minversion " + version)
         assert exit_code > 0
         assert "ERROR: ocrd/core is too old" in err
+
+    def test_bashlib_cp_processor(self):
+        tool = {
+          "executable": "ocrd-cp",
+          "description": "dummy processor copying",
+          "steps": ["preprocessing/optimization"],
+          "categories": ["Image preprocessing"],
+          "parameters": {"message": {
+            "type": "string",
+            "default": "",
+            "description": "message to print on stdout"
+          }}
+        }
+        script = """#!/bin/bash
+        set -eu
+        set -o pipefail
+        MIMETYPE_PAGE=$(ocrd bashlib constants MIMETYPE_PAGE)
+        source $(ocrd bashlib filename)
+        ocrd__wrap ocrd-tool.json ocrd-cp "$@"
+        out_file_grp=${ocrd__argv[output_file_grp]}
+        message="${params[message]}"
+        cd "${ocrd__argv[working_dir]}"
+        mets=$(basename ${ocrd__argv[mets_file]})
+        for ((n=0; n<${#ocrd__files[*]}; n++)); do
+            local in_fpath=($(ocrd__input_file $n url))
+            local in_id=($(ocrd__input_file $n ID))
+            local in_mimetype=($(ocrd__input_file $n mimetype))
+            local in_pageId=($(ocrd__input_file $n pageId))
+            local out_id=$(ocrd__input_file $n outputFileId)
+            local out_fpath="${ocrd__argv[output_file_grp]}/${out_id}.xml
+            if ! test -f "${in_fpath#file://}"; then
+                ocrd__log error "input file '${in_fpath#file://}' (ID=${in_id} pageId=${in_pageId} MIME=${in_mimetype}) is not on disk"
+                continue
+            fi
+            if [ "x${in_mimetype}" = x${MIMETYPE_PAGE} ]; then
+                ocrd__log info "processing PAGE-XML input file $in_id ($in_pageId)"
+            else
+                ocrd__log info "processing ${in_mimetype} input file $in_id ($in_pageId)"
+            fi
+            declare -a options
+            if [[ "${ocrd__argv[overwrite]}" == true ]]; then
+                options=( --force )
+            else
+                options=()
+            fi
+            mkdir -p $out_file_grp
+            cp $options "$in_fpath" "$out_fpath"
+            if [ -n "$message" ]; then
+                echo "$message"
+            fi
+            if [ -n "$in_pageId" ]; then
+                options+=( -g $in_pageId )
+            fi
+            options+=( -G $out_file_grp -m $in_mimetype -i "$out_id" "$out_fpath" )
+            ocrd -l ${ocrd__argv[log_level]} workspace add "${options[@]}"
+        done
+        """
+        with copy_of_directory(assets.path_to('kant_aufklaerung_1784/data')) as wsdir:
+            with pushd_popd(wsdir):
+                json.dump(tool, open('ocrd-tool.json'))
+                exit_code, out, err = self.invoke_bash(
+                    script, '-I', 'OCR-D-GT-PAGE', '-O', 'OCR-D-GT-PAGE2', '-P', 'message', 'hello world')
+                assert exit_code == 0
+                assert 'hello world' in out
+                assert os.path.isdir('OCR-D-GT-PAGE2')
 
 if __name__ == "__main__":
     main(__file__)
