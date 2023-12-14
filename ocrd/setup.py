12237f4d3bdc9ada2f0304e32999051de878d16b
Konstantin Baierer <unixprog@gmail.com>
2023-12-14 20:13:12 +0100

    setup.py/finalize_options: ensure version is assigned only once

--- ocrd/setup.py
+++ ocrd/setup.py
@@ -6,7 +6,7 @@ class build(orig_build):
     def finalize_options(self):
         vers = ' == ' + self.distribution.metadata.version
         self.distribution.install_requires = [
-            req + vers if req.startswith('ocrd') else req
+            req + vers if req.startswith('ocrd') and '==' not in req else req
             for req in self.distribution.install_requires
         ]
         orig_build.finalize_options(self)
