c063a67f138424d3625547116b0f2027370bd805
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-01-19 13:13:04 +0100

    fix processing server tests

--- tests/network/test_processing_server.py
+++ tests/network/test_processing_server.py
@@ -9,10 +9,17 @@ def test_processing_server_connectivity():
     response = get(test_url)
     assert response.status_code == 200, \
         f'Processing server is not reachable on: {test_url}, {response.status_code}'
+    message = response.json()['message']
+    assert message.startsWith('The home page of'), \
+        f'Processing server home page message is corrupted'
 
 
-def _test_processing_server_deployed_processors():
+# TODO: The processing workers are still not registered when deployed separately.
+#  Fix that by extending the processing server.
+def test_processing_server_deployed_processors():
     test_url = f'{PROCESSING_SERVER_URL}/processor'
     response = get(test_url)
+    processors = response.json()
     assert response.status_code == 200, \
         f'Processing server is not reachable on: {test_url}, {response.status_code}'
+    assert processors == [], f'Mismatch in deployed processors'
