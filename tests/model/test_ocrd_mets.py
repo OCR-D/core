4169579505d232f5deecc56468a53cf9cf03c6aa
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2024-02-23 22:56:19 +0100

    test_ocrd_mets.py: also test empty for_fileIds list

--- tests/model/test_ocrd_mets.py
+++ tests/model/test_ocrd_mets.py
@@ -133,6 +133,10 @@ def test_physical_pages_for_fileids(sbb_directory_ocrd_mets):
     assert sbb_directory_ocrd_mets.get_physical_pages(
         for_fileIds=['FILE_0002_IMAGE']) == ['PHYS_0002']
 
+def test_physical_pages_for_emtpy_fileids(sbb_directory_ocrd_mets):
+    assert sbb_directory_ocrd_mets.get_physical_pages(
+        for_fileIds=[]) == []
+
 
 def test_add_group():
     mets = OcrdMets.empty_mets()
