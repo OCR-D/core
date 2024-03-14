027326cf7de54ff73c4145a3ac530b461c70bee8
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-03-14 16:24:24 +0100

    add bash lib processor test

--- tests/network/test_modules_process_helpers.py
+++ tests/network/test_modules_process_helpers.py
@@ -8,9 +8,31 @@ from tests.base import assets
 
 
 def test_invoke_processor_bash():
-    # TODO: Requires locally installed bash lib processor
-    # Check here: `tests/data/bashlib_cp_processor.sh`
     pass
+    bash_lib_cp_processor = "tests/data/bashlib_cp_processor.sh"
+    assert Path(bash_lib_cp_processor).exists(), f"Bash lib test processor not found in: {bash_lib_cp_processor}"
+
+    workspace_root = "kant_aufklaerung_1784/data"
+    path_to_mets = assets.path_to(f"{workspace_root}/mets.xml")
+    assert Path(path_to_mets).exists()
+    log_dir_root = get_root_logging_dir(module_name=NetworkLoggingDirs.PROCESSING_JOBS)
+    job_id = generate_id()
+    path_to_log_file = Path(log_dir_root, job_id)
+    input_file_grp = "OCR-D-IMG"
+    output_file_grp = f"OCR-D-BASH-TEST-{job_id}"
+    invoke_processor(
+        processor_class=None,  # required only for pythonic processors
+        executable=bash_lib_cp_processor,
+        abs_path_to_mets=path_to_mets,
+        input_file_grps=[input_file_grp],
+        output_file_grps=[output_file_grp],
+        page_id="PHYS_0017,PHYS_0020",
+        parameters={},
+        log_filename=path_to_log_file,
+        log_level="DEBUG"
+    )
+    assert Path(assets.path_to(f"{workspace_root}/{output_file_grp}")).exists()
+    assert Path(path_to_log_file).exists()
 
 
 def test_invoke_processor_pythonic():
@@ -20,12 +42,13 @@ def test_invoke_processor_pythonic():
     log_dir_root = get_root_logging_dir(module_name=NetworkLoggingDirs.PROCESSING_JOBS)
     job_id = generate_id()
     path_to_log_file = Path(log_dir_root, job_id)
+    input_file_grp = "OCR-D-IMG"
     output_file_grp = f"OCR-D-DUMMY-TEST-{job_id}"
     invoke_processor(
         processor_class=DummyProcessor,
         executable="",  # not required for pythonic processors
         abs_path_to_mets=path_to_mets,
-        input_file_grps=["OCR-D-IMG"],
+        input_file_grps=[input_file_grp],
         output_file_grps=[output_file_grp],
         page_id="PHYS_0017,PHYS_0020",
         parameters={},
