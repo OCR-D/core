102b3c1b7da6bf3990c980e55d925d8f8f50ff24
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-03-14 16:52:48 +0100

    fix: state -> status

--- src/ocrd_network/models/messages.py
+++ src/ocrd_network/models/messages.py
@@ -7,16 +7,16 @@ class PYResultMessage(BaseModel):
     """ Wraps the parameters required to make a result message request
     """
     job_id: str
-    state: JobState = JobState.unset
+    status: JobState = JobState.unset
     path_to_mets: Optional[str] = None
     workspace_id: Optional[str] = None
 
     class Config:
         schema_extra = {
-            'example': {
-                'job_id': '123123123',
-                'state': JobState.success,
-                'path_to_mets': '/path/to/mets.xml',
-                'workspace_id': 'c7f25615-fc17-4365-a74d-ad20e1ddbd0e'
+            "example": {
+                "job_id": "d8e36726-ed28-5476-b83c-bc31d2eecf1f",
+                "status": JobState.success,
+                "path_to_mets": "/path/to/mets.xml",
+                "workspace_id": "c7f25615-fc17-4365-a74d-ad20e1ddbd0e"
             }
         }
