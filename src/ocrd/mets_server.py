d031dd8d6017e20dbf011ec506db4c4b852dc02a
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-05-03 18:46:42 +0200

    fix ws_dir_path, add all multiplexing requests

--- src/ocrd/mets_server.py
+++ src/ocrd/mets_server.py
@@ -116,11 +116,11 @@ class ClientSideOcrdMets:
     :py:class:`ocrd.mets_server.OcrdMetsServer`.
     """
 
-    def __init__(self, url, mets_target: Optional[str] = None):
+    def __init__(self, url, workspace_path: Optional[str] = None):
         self.protocol = 'tcp' if url.startswith('http://') else 'uds'
         self.log = getLogger(f'ocrd.mets_client[{url}]')
         self.url = url if self.protocol == 'tcp' else f'http+unix://{url.replace("/", "%2F")}'
-        self.mets_target = mets_target if mets_target else None
+        self.ws_dir_path = workspace_path if workspace_path else None
 
         # Set if communication with the OcrdMetsServer happens over the ProcessingServer
         # The received root URL must be in the form: http://PS_host:PS_port/tcp_mets
@@ -144,7 +144,7 @@ class ClientSideOcrdMets:
             self.session.request(method='PUT', url=self.url)
             return
         request_body = {
-            "mets_path": "",
+            "workspace_path": self.ws_dir_path,
             "method_type": "PUT",
             "request_url": "",
             "request_data": {}
@@ -160,7 +160,7 @@ class ClientSideOcrdMets:
                 self.session.request(method='DELETE', url=self.url)
                 return
             request_body = {
-                "mets_path": "",
+                "workspace_path": self.ws_dir_path,
                 "method_type": "DELETE",
                 "request_url": "",
                 "request_data": {}
@@ -176,9 +176,8 @@ class ClientSideOcrdMets:
         """
         if not self.multiplexing_mode:
             return self.session.request(method='POST', url=f'{self.url}/reload').text
-
         request_body = {
-            "mets_path": "",
+            "workspace_path": self.ws_dir_path,
             "method_type": "POST",
             "request_url": "reload",
             "request_data": {}
@@ -187,27 +186,71 @@ class ClientSideOcrdMets:
 
     @property
     def unique_identifier(self):
-        return self.session.request(method='GET', url=f'{self.url}/unique_identifier').text
+        if not self.multiplexing_mode:
+            return self.session.request(method='GET', url=f'{self.url}/unique_identifier').text
+        request_body = {
+            "workspace_path": self.ws_dir_path,
+            "method_type": "GET",
+            "request_url": "unique_identifier",
+            "request_data": {}
+        }
+        return self.session.request(method="POST", url=self.url, json=request_body).text
 
     @property
     def workspace_path(self):
-        if not self.mets_target:
-            self.mets_target = self.session.request(method='GET', url=f'{self.url}/workspace_path').text
-        return self.mets_target
+        if self.ws_dir_path:
+            return self.ws_dir_path
+        if not self.multiplexing_mode:
+            self.ws_dir_path = self.session.request(method='GET', url=f'{self.url}/workspace_path').text
+            return self.ws_dir_path
+        request_body = {
+            "workspace_path": self.ws_dir_path,
+            "method_type": "GET",
+            "request_url": "workspace_path",
+            "request_data": {}
+        }
+        self.ws_dir_path = self.session.request(method="POST", url=self.url, json=request_body).text
+        return self.ws_dir_path
 
     @property
     def file_groups(self):
-        return self.session.request(method='GET', url=f'{self.url}/file_groups').json()['file_groups']
+        if not self.multiplexing_mode:
+            return self.session.request(method='GET', url=f'{self.url}/file_groups').json()['file_groups']
+        request_body = {
+            "workspace_path": self.ws_dir_path,
+            "method_type": "GET",
+            "request_url": "file_groups",
+            "request_data": {}
+        }
+        return self.session.request(method="POST", url=self.url, json=request_body).json()['file_groups']
 
     @property
     def agents(self):
-        agent_dicts = self.session.request(method='GET', url=f'{self.url}/agent').json()['agents']
+        if not self.multiplexing_mode:
+            agent_dicts = self.session.request(method='GET', url=f'{self.url}/agent').json()['agents']
+        else:
+            request_body = {
+                "workspace_path": self.ws_dir_path,
+                "method_type": "GET",
+                "request_url": "agent",
+                "request_data": {}
+            }
+            agent_dicts = self.session.request(method="POST", url=self.url, json=request_body).json()['agents']
         for agent_dict in agent_dicts:
             agent_dict['_type'] = agent_dict.pop('type')
         return [ClientSideOcrdAgent(None, **agent_dict) for agent_dict in agent_dicts]
 
     def add_agent(self, *args, **kwargs):
-        return self.session.request(method='POST', url=f'{self.url}/agent', json=OcrdAgentModel.create(**kwargs).dict())
+        if not self.multiplexing_mode:
+            return self.session.request(
+                method='POST', url=f'{self.url}/agent', json=OcrdAgentModel.create(**kwargs).dict())
+        request_body = {
+            "workspace_path": self.ws_dir_path,
+            "method_type": "POST",
+            "request_url": "agent",
+            "request_data": OcrdAgentModel.create(**kwargs).dict()
+        }
+        return self.session.request(method="POST", url=self.url, json=request_body)
 
     @deprecated_alias(ID="file_id")
     @deprecated_alias(pageId="page_id")
@@ -220,7 +263,20 @@ class ClientSideOcrdMets:
             kwargs['file_id'] = kwargs.pop('ID')
         if 'fileGrp' in kwargs:
             kwargs['file_grp'] = kwargs.pop('fileGrp')
-        r = self.session.request(method='GET', url=f'{self.url}/file', params={**kwargs})
+
+        if not self.multiplexing_mode:
+            r = self.session.request(method='GET', url=f'{self.url}/file', params={**kwargs})
+        else:
+            request_body = {
+                "workspace_path": self.ws_dir_path,
+                "method_type": "GET",
+                "request_url": "file",
+                "request_data": {
+                    "params": {**kwargs}
+                }
+            }
+            r = self.session.request(method="POST", url=self.url, json=request_body)
+
         for f in r.json()['files']:
             yield ClientSideOcrdFile(
                 None, ID=f['file_id'], pageId=f['page_id'], fileGrp=f['file_grp'], url=f['url'],
@@ -239,7 +295,19 @@ class ClientSideOcrdMets:
             file_id=file_id, file_grp=file_grp, page_id=page_id, mimetype=mimetype, url=url,
             local_filename=local_filename
         )
-        r = self.session.request(method='POST', url=f'{self.url}/file', data=data.dict())
+
+        if not self.multiplexing_mode:
+            r = self.session.request(method='POST', url=f'{self.url}/file', data=data.dict())
+        else:
+            request_body = {
+                "workspace_path": self.ws_dir_path,
+                "method_type": "POST",
+                "request_url": "file",
+                "request_data": {
+                    "data": data.dict()
+                }
+            }
+            r = self.session.request(method="POST", url=self.url, json=request_body)
         return ClientSideOcrdFile(
             None, ID=file_id, fileGrp=file_grp, url=url, pageId=page_id, mimetype=mimetype,
             local_filename=local_filename
