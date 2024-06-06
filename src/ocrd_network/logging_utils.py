96a56003a1ea06879273ed6af086128fdc34ad45
kba <unixprog@gmail.com>
2024-06-06 14:57:24 +0200

    Revert "change permisisons to 774 and 664"
    This reverts commit 2c4a3fd1e127042d5c1fea8410aa8579e626c158.

--- src/ocrd_network/logging_utils.py
+++ src/ocrd_network/logging_utils.py
@@ -10,7 +10,7 @@ def configure_file_handler_with_formatter(logger: Logger, log_file: Path, mode: 
     file_handler.setFormatter(Formatter(LOG_FORMAT))
     logger.addHandler(file_handler)
     try:
-        log_file.chmod(0o664)
+        log_file.chmod(0o666)
     except PermissionError:
         # if the file exists the permissions are supposed to already fit
         pass
@@ -20,7 +20,7 @@ def get_root_logging_dir(module_name: NetworkLoggingDirs) -> Path:
     module_log_dir = Path(config.OCRD_NETWORK_LOGS_ROOT_DIR, module_name.value)
     try:
         module_log_dir.mkdir(parents=True, exist_ok=True)
-        module_log_dir.chmod(0o774)
+        module_log_dir.chmod(0o777)
     except PermissionError:
         # if the folder exists the permissions are supposed to already fit
         pass
