224dfc5098e9912b9c2bf87f851a52e79b51250b
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-06-24 14:00:32 +0200

    Processor: refactor processing API…
    - add method `process_workspace(workspace)`
      as a replacement for passing `workspace`
      in the constructor and then calling `process`
      (implemented by subclasses): implement in the
      superclass
      - loop over input files
      - delegate processing to new method
        `process_page_file()` if possible
      - otherwise fall back to old `process()`
        outside of loop
      - download input files when needed
        if `self.download`
    - add method `process_page_file()` as single-page
      processing procedure on OcrdFiles: implement
      in the superclass for the most frequent/default
      use-case of
      - (multi-) image/PAGE input files
      - (single) PAGE output files
      - delegate to new method `process_page_pcgts()`
        if available
      - add PAGE processing metadata
      - set PAGE PcGtsId
      - handle `make_file_id` and `workspace.add_file`
    - add method `process_page_pcgts()` as single-page
      processing function on OcrdPage: to be implemented
      only by subclasses
    - constructor: add kwarg `download_files` controlling
      `self.download` (see above)

--- src/ocrd/processor/base.py
+++ src/ocrd/processor/base.py
@@ -15,6 +15,7 @@ import json
 import os
 from os import getcwd
 from pathlib import Path
+from typing import Optional
 import sys
 import tarfile
 import io
@@ -32,9 +33,11 @@ from ocrd_utils import (
     list_all_resources,
     get_processor_resource_types,
     resource_filename,
+    make_file_id,
 )
 from ocrd_validators import ParameterValidator
-from ocrd_models.ocrd_page import MetadataItemType, LabelType, LabelsType
+from ocrd_models.ocrd_page import MetadataItemType, LabelType, LabelsType, OcrdPage, to_xml
+from ocrd_modelfactory import page_from_file
 
 # XXX imports must remain for backwards-compatibility
 from .helpers import run_cli, run_processor, generate_processor_help # pylint: disable=unused-import
@@ -64,12 +67,15 @@ class Processor():
 
     def __init__(
             self,
-            workspace : Workspace,
+            # FIXME: deprecate in favor of process_workspace(workspace)
+            workspace : Optional[Workspace],
             ocrd_tool=None,
             parameter=None,
             input_file_grp=None,
             output_file_grp=None,
             page_id=None,
+            download_files=True,
+            # FIXME: deprecate all the following in favor of respective methods
             resolve_resource=None,
             show_resource=None,
             list_resources=False,
@@ -99,6 +105,7 @@ class Processor():
              output_file_grp (string): comma-separated list of METS ``fileGrp``s used for output.
              page_id (string): comma-separated list of METS physical ``page`` IDs to process \
                  (or empty for all pages).
+             download_files (boolean): Whether input files will be downloaded prior to processing.
              resolve_resource (string): If not ``None``, then instead of processing, resolve \
                  given resource by name and print its full path to stdout.
              show_resource (string): If not ``None``, then instead of processing, resolve \
@@ -154,15 +161,17 @@ class Processor():
             self.show_version()
             return
         self.workspace = workspace
-        # FIXME HACK would be better to use pushd_popd(self.workspace.directory)
-        # but there is no way to do that in process here since it's an
-        # overridden method. chdir is almost always an anti-pattern.
         if self.workspace:
+            # FIXME deprecate setting this and calling process() over using process_workspace()
+            # which uses pushd_popd(self.workspace.directory)
+            # (because there is no way to do that in process() since it's an
+            # overridden method. chdir is almost always an anti-pattern.)
             self.old_pwd = getcwd()
             os.chdir(self.workspace.directory)
         self.input_file_grp = input_file_grp
         self.output_file_grp = output_file_grp
         self.page_id = None if page_id == [] or page_id is None else page_id
+        self.download = download_files
         parameterValidator = ParameterValidator(ocrd_tool)
         report = parameterValidator.validate(parameter)
         if not report.is_valid:
@@ -197,17 +206,109 @@ class Processor():
     @deprecated(version='3.0', reason='process() should be replaced with process_page() and process_workspace()')
     def process(self) -> None:
         """
-        Process the :py:attr:`workspace` 
+        Process all files of the :py:attr:`workspace` 
         from the given :py:attr:`input_file_grp`
         to the given :py:attr:`output_file_grp`
-        for the given :py:attr:`page_id`
+        for the given :py:attr:`page_id` (or all pages)
         under the given :py:attr:`parameter`.
         
         (This contains the main functionality and needs to be overridden by subclasses.)
         """
         raise NotImplementedError()
 
-    def add_metadata(self, pcgts):
+    def process_workspace(self, workspace: Workspace) -> None:
+        """
+        Process all files of the given ``workspace``,
+        from the given :py:attr:`input_file_grp`
+        to the given :py:attr:`output_file_grp`
+        for the given :py:attr:`page_id` (or all pages)
+        under the given :py:attr:`parameter`.
+
+        (This will iterate over pages and files, calling
+        :py:meth:`process_page`, handling exceptions.)
+        """
+        # assert self.input_file_grp is not None
+        # assert self.output_file_grp is not None
+        # input_file_grps = self.input_file_grp.split(',')
+        # for input_file_grp in input_file_grps:
+        #     assert input_file_grp in workspace.mets.file_groups
+        log = getLogger('ocrd.processor.base')
+        with pushd_popd(workspace.directory):
+            self.workspace = workspace
+            try:
+                # FIXME: add page parallelization by running multiprocessing.Pool (#322)
+                for input_file_tuple in self.zip_input_files(on_error='abort'):
+                    # FIXME: add error handling by catching exceptions in various ways (#579)
+                    # for example:
+                    # - ResourceNotFoundError → use ResourceManager to download (once), then retry
+                    # - transient (I/O or OOM) error → maybe sleep, retry
+                    # - persistent (data) error → skip / dummy / raise
+                    input_files = [None] * len(input_file_tuple)
+                    for i, input_file in enumerate(input_file_tuple):
+                        if i == 0:
+                            log.info("processing page %s", input_file.pageId)
+                        elif input_file is None:
+                            # file/page not found in this file grp
+                            continue
+                        input_files[i] = input_file
+                        if not self.download:
+                            continue
+                        try:
+                            input_files[i] = self.workspace.download_file(input_file)
+                        except ValueError as e:
+                            log.error(repr(e))
+                            log.warning("skipping file %s for page %s", input_file, input_file.pageId)
+                    self.process_page_file(*input_files)
+            except NotImplementedError:
+                # fall back to deprecated method
+                self.process()
+
+    def process_page_file(self, *input_files) -> None:
+        """
+        Process the given ``input_files`` of the :py:attr:`workspace`,
+        representing one physical page (passed as one opened
+        :py:class:`~ocrd_models.OcrdFile` per input fileGrp)
+        under the given :py:attr:`parameter`, and make sure the
+        results get added accordingly.
+
+        (This uses process_page_pcgts, but can be overridden by subclasses
+        to handle cases like multiple fileGrps, non-PAGE input etc.)
+        """
+        log = getLogger('ocrd.processor.base')
+        input_pcgts = [None] * len(input_files)
+        for i, input_file in enumerate(input_files):
+            # FIXME: what about non-PAGE input like image or JSON ???
+            log.debug("parsing file %s for page %s", input_file.ID, input_file.pageId)
+            try:
+                input_pcgts[i] = page_from_file(input_file)
+            except ValueError as e:
+                log.info("non-PAGE input for page %s: %s", input_file.pageId, e)
+        output_pcgts = self.process_page_pcgts(*input_pcgts)
+        output_file_id = make_file_id(input_files[0], self.output_file_grp)
+        output_pcgts.set_pcGtsId(output_file_id)
+        self.add_metadata(output_pcgts)
+        # FIXME: what about save_image_file in process_page ???
+        # FIXME: what about non-PAGE output like JSON ???
+        self.workspace.add_file(file_id=output_file_id,
+                                file_grp=self.output_file_grp,
+                                page_id=input_files[0].pageId,
+                                local_filename=os.path.join(self.output_file_grp, output_file_id + '.xml'),
+                                mimetype=MIMETYPE_PAGE,
+                                content=to_xml(output_pcgts))
+
+    def process_page_pcgts(self, *input_pcgts) -> OcrdPage:
+        """
+        Process the given ``input_pcgts`` of the :py:attr:`workspace`,
+        representing one physical page (passed as one parsed
+        :py:class:`~ocrd_models.OcrdPage` per input fileGrp)
+        under the given :py:attr:`parameter`, and return the
+        resulting :py:class:`~ocrd_models.OcrdPage`.
+
+        (This contains the main functionality and must be overridden by subclasses.)
+        """
+        raise NotImplementedError()
+
+    def add_metadata(self, pcgts: OcrdPage) -> None:
         """
         Add PAGE-XML :py:class:`~ocrd_models.ocrd_page.MetadataItemType` ``MetadataItem`` describing
         the processing step and runtime parameters to :py:class:`~ocrd_models.ocrd_page.PcGtsType` ``pcgts``.
