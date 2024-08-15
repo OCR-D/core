061f0231a148f09943d1c5ee35f456ad502f2755
kba <unixprog@gmail.com>
2024-08-15 17:34:43 +0200

    allow "from ocrd_models import OcrdPage

--- src/ocrd_models/__init__.py
+++ src/ocrd_models/__init__.py
@@ -5,5 +5,6 @@ from .ocrd_agent import OcrdAgent, ClientSideOcrdAgent
 from .ocrd_exif import OcrdExif
 from .ocrd_file import OcrdFile, ClientSideOcrdFile
 from .ocrd_mets import OcrdMets
+from .ocrd_page import OcrdPage
 from .ocrd_xml_base import OcrdXmlDocument
 from .report import ValidationReport
