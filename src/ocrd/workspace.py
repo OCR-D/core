be6b59d03903e3da153e4d64080f35a07fb5082a
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-08-28 15:14:00 +0200

    Processor: fix ignore (negative/zero) cases for max_workers / max_page_seconds

--- src/ocrd/workspace.py
+++ src/ocrd/workspace.py
@@ -95,8 +95,8 @@ class Workspace():
             if self.is_remote:
                 mets = ClientSideOcrdMets(mets_server_url, self.directory)
                 if mets.workspace_path != self.directory:
-                    raise ValueError(f"METS server {mets_server_url} workspace directory {mets.workspace_path} differs "
-                            f"from local workspace directory {self.directory}. These are not the same workspaces.")
+                    raise ValueError(f"METS server {mets_server_url} workspace directory '{mets.workspace_path}' differs "
+                            f"from local workspace directory '{self.directory}'. These are not the same workspaces.")
             else:
                 mets = OcrdMets(filename=self.mets_target)
         self.mets = mets
