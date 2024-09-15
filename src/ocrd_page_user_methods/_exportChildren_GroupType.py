c47ae77cf57b2fbe4227cc3c9541de5a7d0f1031
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-09-15 23:50:45 +0200

    OcrdPage: migrate to newest generateds, adapt user methods, re-generate

--- src/ocrd_page_user_methods/_exportChildren_GroupType.py
+++ src/ocrd_page_user_methods/_exportChildren_GroupType.py
@@ -1,9 +1,14 @@
 # pylint: disable=line-too-long,invalid-name,missing-module-docstring,missing-function-docstring
-def exportChildren(self, outfile, level, namespaceprefix_='', namespacedef_='xmlns:pc="http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15"', name_='OrderedGroupType', fromsubclass_=False, pretty_print=True): # pylint: disable=unused-argument,too-many-arguments
-    namespaceprefix_ = 'pc:'
+def _exportChildren(self, outfile, level, namespaceprefix_='', namespacedef_='xmlns:pc="http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15"', name_='OrderedGroupType', fromsubclass_=False, pretty_print=True): # pylint: disable=unused-argument,too-many-arguments
+    if pretty_print:
+        eol_ = '\n'
+    else:
+        eol_ = ''
     if self.UserDefined is not None:
+        namespaceprefix_ = self.UserDefined_nsprefix_ + ':' if (UseCapturedNS_ and self.UserDefined_nsprefix_) else ''
         self.UserDefined.export(outfile, level, namespaceprefix_, namespacedef_='', name_='UserDefined', pretty_print=pretty_print)
     for Labels_ in self.Labels:
+        namespaceprefix_ = self.Labels_nsprefix_ + ':' if (UseCapturedNS_ and self.Labels_nsprefix_) else ''
         Labels_.export(outfile, level, namespaceprefix_, namespacedef_='', name_='Labels', pretty_print=pretty_print)
     cleaned = []
     def replaceWithRRI(group):
@@ -21,4 +26,4 @@ def exportChildren(self, outfile, level, namespaceprefix_='', namespacedef_='xml
         else:
             cleaned.append(entry)
     for entry in cleaned:
-        entry.export(outfile, level, namespaceprefix_, namespacedef_='', name_=entry.__class__.__name__[:-4], pretty_print=pretty_print)
+        entry.export(outfile, level, entry.ns_prefix_, namespacedef_='', name_=entry.__class__.__name__[:-4], pretty_print=pretty_print)
