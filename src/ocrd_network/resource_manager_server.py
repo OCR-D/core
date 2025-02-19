4f2ff6c0d4e7afeaf2de639b09ee567d2ae026e8
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2025-02-19 17:29:30 +0100

    implement list available and list installed

--- src/ocrd_network/resource_manager_server.py
+++ src/ocrd_network/resource_manager_server.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 from os import getpid
+from typing import Any
 from uvicorn import run as uvicorn_run
 from fastapi import APIRouter, FastAPI, status
+
+from ocrd import OcrdResourceManager
 from ocrd_utils import initLogging, getLogger
 from .logging_utils import configure_file_handler_with_formatter, get_resource_manager_server_logging_file_path
 
@@ -19,6 +22,8 @@ class ResourceManagerServer(FastAPI):
         self.log = getLogger("ocrd_network.resource_manager_server")
         log_file = get_resource_manager_server_logging_file_path(pid=getpid())
         configure_file_handler_with_formatter(self.log, log_file=log_file, mode="a")
+
+        self.resmgr_instance = OcrdResourceManager()
 
         self.hostname = host
         self.port = port
@@ -53,6 +58,27 @@ class ResourceManagerServer(FastAPI):
             status_code=status.HTTP_200_OK,
             summary="Get information about the OCR-D Resource Manager Server"
         )
+        base_router.add_api_route(
+            path="/list_available",
+            endpoint=self.list_available_models,
+            methods=["GET"],
+            status_code=status.HTTP_200_OK,
+            summary=""
+        )
+        base_router.add_api_route(
+            path="/list_installed",
+            endpoint=self.list_installed_models,
+            methods=["GET"],
+            status_code=status.HTTP_200_OK,
+            summary=""
+        )
+        base_router.add_api_route(
+            path="/download",
+            endpoint=self.download_model,
+            methods=["GET"],
+            status_code=status.HTTP_200_OK,
+            summary=""
+        )
         self.include_router(base_router)
 
     async def home_page(self):
@@ -62,3 +88,20 @@ class ResourceManagerServer(FastAPI):
             "time": datetime.now().strftime("%Y-%m-%d %H:%M")
         }
         return json_message
+
+    async def list_available_models(self, executable: Any = None, dynamic: bool = True, name: Any = None):
+        result = self.resmgr_instance.list_available(executable, dynamic, name)
+        json_message = {
+            "result": result
+        }
+        return json_message
+
+    async def list_installed_models(self, executable: Any = None):
+        result = self.resmgr_instance.list_available(executable)
+        json_message = {
+            "result": result
+        }
+        return json_message
+
+    async def download_model(self):
+        pass
