5844e97efbb4bf31c808b9c40fbd641bf6cfe559
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2025-03-04 14:44:13 +0100

    implement proper download method

--- src/ocrd_network/resource_manager_server.py
+++ src/ocrd_network/resource_manager_server.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 from os import getpid
+from pathlib import Path
+import requests
+from shutil import which
 from typing import Any
 from uvicorn import run as uvicorn_run
 from fastapi import APIRouter, FastAPI, HTTPException, status
 
 from ocrd import OcrdResourceManager
-from ocrd.cli.resmgr import resmgr_cli
-from ocrd_utils import initLogging, getLogger, RESOURCE_LOCATIONS
+from ocrd_utils import directory_size, getLogger, get_moduledir, get_ocrd_tool_json, initLogging
 from .logging_utils import configure_file_handler_with_formatter, get_resource_manager_server_logging_file_path
 
 
@@ -105,7 +107,7 @@ class ResourceManagerServer(FastAPI):
     @staticmethod
     async def download_resource(
         executable: str,
-        resource_name: Any = None,
+        name: Any = None,
         location: Any = None,
         any_url: str = '',
         no_dynamic: bool = False,
@@ -114,18 +116,101 @@ class ResourceManagerServer(FastAPI):
         allow_uninstalled: bool = True,
         overwrite: bool = True
     ):
-        resmgr_cli.download(
-            any_url=any_url,
-            no_dynamic=no_dynamic,
-            resource_type=resource_type,
-            path_in_archive=path_in_archive,
-            allow_uninstalled=allow_uninstalled,
-            overwrite=overwrite,
-            location=location,
-            executable=executable,
-            name=resource_name
-        )
+        log = getLogger('ocrd.cli.resmgr')
+        resmgr = OcrdResourceManager()
+        response = []
+        if executable != '*' and not name:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail=f"Unless EXECUTABLE ('{executable}') is the '*' wildcard, NAME is required"
+            )
+        elif executable == '*':
+            executable = None
+        if name == '*':
+            name = None
+        is_url = (any_url.startswith('https://') or any_url.startswith('http://')) if any_url else False
+        is_filename = Path(any_url).exists() if any_url else False
+        if executable and not which(executable):
+            if not allow_uninstalled:
+                raise HTTPException(
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                    detail=f"Executable '{executable}' is not installed. "
+                           f"To download resources anyway, use the -a/--allow-uninstalled flag"
+                )
+            else:
+                response.append(f"Executable '{executable}' is not installed, but downloading resources anyway.")
+        reslist = resmgr.list_available(executable=executable, dynamic=not no_dynamic, name=name)
+        if not any(r[1] for r in reslist):
+            response.append(f"No resources {name} found in registry for executable {executable}")
+            if executable and name:
+                reslist = [(executable, [{
+                    'url': any_url or '???',
+                    'name': name,
+                    'type': resource_type,
+                    'path_in_archive': path_in_archive}]
+                            )]
+        for this_executable, this_reslist in reslist:
+            for resdict in this_reslist:
+                if 'size' in resdict:
+                    registered = "registered"
+                else:
+                    registered = "unregistered"
+                if any_url:
+                    resdict['url'] = any_url
+                if resdict['url'] == '???':
+                    response.append(f"Cannot download user resource {resdict['name']}")
+                    continue
+                if resdict['url'].startswith('https://') or resdict['url'].startswith('http://'):
+                    response.append(f"Downloading {registered} resource '{resdict['name']}' ({resdict['url']})")
+                    if 'size' not in resdict:
+                        with requests.head(resdict['url']) as r:
+                            resdict['size'] = int(r.headers.get('content-length', 0))
+                else:
+                    response.append(f"Copying {registered} resource '{resdict['name']}' ({resdict['url']})")
+                    urlpath = Path(resdict['url'])
+                    resdict['url'] = str(urlpath.resolve())
+                    if Path(urlpath).is_dir():
+                        resdict['size'] = directory_size(urlpath)
+                    else:
+                        resdict['size'] = urlpath.stat().st_size
+                if not location:
+                    location = get_ocrd_tool_json(this_executable)['resource_locations'][0]
+                elif location not in get_ocrd_tool_json(this_executable)['resource_locations']:
+                    raise HTTPException(
+                        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                        detail=f"The selected --location {location} is not in the {this_executable}'s resource search "
+                               f"path, refusing to install to invalid location"
+                    )
+                if location != 'module':
+                    basedir = resmgr.location_to_resource_dir(location)
+                else:
+                    basedir = get_moduledir(this_executable)
+                    if not basedir:
+                        basedir = resmgr.location_to_resource_dir('data')
+                try:
+                    fpath = resmgr.download(
+                        this_executable,
+                        resdict['url'],
+                        basedir,
+                        name=resdict['name'],
+                        resource_type=resdict.get('type', resource_type),
+                        path_in_archive=resdict.get('path_in_archive', path_in_archive),
+                        overwrite=overwrite,
+                        no_subdir=location in ['cwd', 'module'],
+                        progress_cb=None
+                    )
+                    if registered == 'unregistered':
+                        response.append(f"{this_executable} resource '{name}' ({any_url}) not a known resource, "
+                                        f"creating stub in {resmgr.user_list}'")
+                        resmgr.add_to_user_database(this_executable, fpath, url=any_url)
+                    resmgr.save_user_list()
+                    response.append(f"Installed resource {resdict['url']} under {fpath}")
+                except FileExistsError as exc:
+                    response.append(str(exc))
+                parameter_usage = resmgr.parameter_usage(
+                    resdict['name'], usage=resdict.get('parameter_usage', 'as-is'))
+                response.append(f"Use in parameters as '{parameter_usage}'")
         json_message = {
-            "result": "Success"
+            "result": response
         }
         return json_message
