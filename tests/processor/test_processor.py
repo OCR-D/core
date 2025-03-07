72074e0f4216a7da369cca0767526058f7b879a3
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2025-03-07 16:16:21 +0100

    test_processor timeouts: more tolerant

--- tests/processor/test_processor.py
+++ tests/processor/test_processor.py
@@ -525,7 +525,7 @@ def test_run_output_parallel(start_mets_server):
                   parameter={"sleep": 2},
                   mets_server_url=mets_server_url)
     run_time = time.time() - start_time
-    assert run_time < 3.2, f"run_processor took {run_time}s"
+    assert run_time < 3.5, f"run_processor took {run_time}s"
     assert len(ws.mets.find_all_files(fileGrp="OCR-D-OUT")) == len(ws.mets.find_all_files(fileGrp="OCR-D-IMG"))
     config.reset_defaults()
 
@@ -547,14 +547,14 @@ def test_run_output_parallel_caching(start_mets_server):
     start_time = time.time()
     proc1 = run_processor(DummyProcessorWithOutputSleep, **kwargs)
     run_time = time.time() - start_time
-    assert run_time < 3.2, f"run_processor took {run_time}s"
+    assert run_time < 3.5, f"run_processor took {run_time}s"
     assert len(ws.mets.find_all_files(fileGrp="OCR-D-OUT")) == len(ws.mets.find_all_files(fileGrp="OCR-D-IMG"))
     start_time = time.time()
     proc2 = run_processor(DummyProcessorWithOutputSleep, **kwargs)
     assert proc1 is proc2, "instance_caching must yield identical processor objects for equal parameters"
     run_time = time.time() - start_time
     # should be faster with default config.OCRD_EXISTING_OUTPUT==SKIP
-    assert run_time < 1.2, f"run_processor took {run_time}s"
+    assert run_time < 1.5, f"run_processor took {run_time}s"
     config.reset_defaults()
 
 if __name__ == "__main__":
