e7dc54a7714ab8f4848b129cede36790e6fa7a48
joschrew <91774427+joschrew@users.noreply.github.com>
2025-09-09 16:08:31 +0200

    Fix test for list-processors

--- tests/network/test_integration_5_processing_server.py
+++ tests/network/test_integration_5_processing_server.py
@@ -20,14 +20,12 @@ def test_processing_server_connectivity():
     assert message.startswith("The home page of"), f"Processing server home page message is corrupted"
 
 
-# TODO: The processing workers are still not registered when deployed separately.
-#  Fix that by extending the processing server.
 def test_processing_server_deployed_processors():
     test_url = f"{PROCESSING_SERVER_URL}/processor"
     response = request_get(test_url)
     processors = response.json()
     assert response.status_code == 200, f"Processing server: {test_url}, {response.status_code}"
-    assert processors == [], f"Mismatch in deployed processors"
+    assert "ocrd-dummy" in processors
 
 
 def test_processing_server_processing_request():
