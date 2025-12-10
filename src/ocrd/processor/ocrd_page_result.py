3a776a12e8f159dbcd36a10cfe4c8ccc61ce88f2
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2025-12-10 17:36:42 +0100

    OcrdPage: allow for copy() and deepcopy()

--- src/ocrd/processor/ocrd_page_result.py
+++ src/ocrd/processor/ocrd_page_result.py
@@ -8,12 +8,39 @@ from ocrd_models.ocrd_page_generateds import AlternativeImageType, PageType
 
 @dataclass
 class OcrdPageResultImage():
+    """
+    Encapsulates a single ``AlternativeImage`` reference to be persisted
+    as image file to the :py:class:`ocrd.Workspace`.
+    """
     pil: Image
+    """
+    image data to be saved
+    """
     file_id_suffix: str
+    """
+    a suffix to append to the file name when saving
+    (something like ``.IMG`` according to OCR-D
+    conventions for PAGE-XML)
+    """
     alternative_image: Optional[Union[AlternativeImageType, PageType]]
+    """
+    the ``AlternativeImage`` instance that references this image;
+    to be amended with the actual (final) ``@filename`` when saving
+
+    alternatively, can be a ``Page`` instance: in that case,
+    amend its ``@imageFilename`` (i.e. replace the original image
+    of the PAGE-XML)
+    """
 
 
 @dataclass
 class OcrdPageResult():
+    """
+    Encapsulates the return type of :py:func:`ocrd.Processor.process_page_pcgts`,
+    i.e. an instance of :py:class:`ocrd_models.ocrd_page.OcrdPage` and an
+    accompanying list of :py:class:`OcrdPageResultImage` that contain all
+    image files referenced via ``AlternativeImage`` to be persisted into the
+    :py:class:`ocrd.Workspace` along with the PAGE-XML itself.
+    """
     pcgts: OcrdPage
     images: List[OcrdPageResultImage] = field(default_factory=list)
